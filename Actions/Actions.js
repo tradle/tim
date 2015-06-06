c74d3f1535e10d13b42701735c94ed9a1be45d08
pgmemk <ellen.katsnelson@lablz.com>
2015-06-06 19:23:55 -0400

    multiple identity project

--- Actions/Actions.js
+++ Actions/Actions.js
@@ -5,9 +5,14 @@ var Reflux = require('reflux');
 var Actions = Reflux.createActions([
   'addItem',        
   'addMessage',
+  'getItem',
+  'addNewIdentity',
   'reloadDB',
   'list',
   'start'
+  // 'messageList',
+  // 'getMe',
+  // 'getDb',
 ]);
 
 module.exports = Actions;
