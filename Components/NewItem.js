fd78b4d8d9f622ba0fe5bbd1b9714221f41580fa
pgmemk <ellen.katsnelson@lablz.com>
2015-06-15 20:50:54 -0400

    allowing to choose photo/photos for resource

--- Components/NewItem.js
+++ Components/NewItem.js
@@ -3,6 +3,9 @@
 var React = require('react-native');
 var t = require('tcomb-form-native');
 var utils = require('../utils/utils');
+var extend = require('extend');
+var logError = require('logError');
+var SelectPhotoList = require('./SelectPhotoList');
 
 var Form = t.form.Form;
 
@@ -16,15 +19,31 @@ var {
 } = React;
 
 class NewItem extends Component {
-  onSavePressed(addMore) {
-    if (this.addItem()) 
-      this.props.navigator.pop();
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedAssets: {}
+    }
   }
-  addItem() {
+  onSavePressed() {
     var value = this.refs.form.getValue();
     if (!value)
       return;
-
+    var propName = this.props.metadata.name;
+    var json = JSON.parse(JSON.stringify(value));
+    if (utils.isEmpty(this.state.selectedAssets)) 
+      this.props.onAddItem(propName, json);
+    else {
+      for (var assetUri in this.state.selectedAssets) {
+        var newJson = {};
+        extend(newJson, json);
+        newJson = {url: assetUri, title: 'photo'};
+        this.props.onAddItem(propName, newJson);    
+      }
+      this.props.navigator.pop();
+    }
+  }
+  addItem() {
     var propName = this.props.metadata.name;
     var json = JSON.parse(JSON.stringify(value));
     this.props.onAddItem(propName, json);    
@@ -33,7 +52,7 @@ class NewItem extends Component {
   render() {
     var props = this.props;
     var parentBG = {backgroundColor: '#7AAAC3'};
-    var err = this.props.err || '';
+    var err = props.err || '';
 
     var meta =  props.metadata;
     var model = {};
@@ -55,30 +74,37 @@ class NewItem extends Component {
         <Text style={errStyle}>{err}</Text>
         <View style={{'padding': 20}}>
           <Form ref='form' type={Model} options={options} />
-          <TouchableHighlight style={[styles.button, parentBG]} underlayColor='#7AAAC3'
-              onPress={this.onSavePressed.bind(this)}>
-            <Text style={[styles.buttonText]}>Save</Text>
-          </TouchableHighlight>
+          <SelectPhotoList 
+            metadata={this.props.metadata} 
+            navigator={this.props.navigator} 
+            onSelect={this.onSelect.bind(this)} />
         </View>
       </View>
       </ScrollView>
     );
   }
-            // <TouchableHighlight style={[styles.button, parentBG, {paddingRight:10, paddingLeft:10}]} underlayColor='#7AAAC3'
-            //     onPress={this.onSavePressed.bind(this, true)}>
-            //   <Text style={[styles.buttonText]}>Save + Add</Text>
-            // </TouchableHighlight>
-            // <TouchableHighlight style={styles.button} underlayColor='#ffffff'
-            //     onPress={this.onCancelPressed.bind(this)}>
-            //   <Text style={[styles.buttonText,{'color':'#2E3B4E'}]}>Cancel</Text>
-            // </TouchableHighlight>
-
+  onSelect(asset) {    
+    var selectedAssets = this.state.selectedAssets;
+    // unselect if was selected before
+    if (selectedAssets[asset.node.image.uri])
+      delete selectedAssets[asset.node.image.uri];
+    else
+      selectedAssets[asset.node.image.uri] = asset;
+  }
 }
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 60
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignSelf: 'center'
   },
   buttons: { 
     flex: 1,
