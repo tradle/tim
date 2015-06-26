7c5a84c8d0c8876e2822f62c5255d7e3ed01b808
pgmemk <ellen.katsnelson@lablz.com>
2015-06-26 00:03:24 -0400

    verifications creation first draft

--- Components/VerificationButton.js
+++ Components/VerificationButton.js
@@ -3,6 +3,11 @@
 var React = require('react-native');
 var utils = require('../utils/utils');
 var Icon = require('FAKIconImage');
+var reactMixin = require('react-mixin');
+var Actions = require('../Actions/Actions');
+var Reflux = require('reflux');
+var Store = require('../Store/Store');
+var reactMixin = require('react-mixin');
 
 var {
   StyleSheet,
@@ -13,6 +18,22 @@ var {
 } = React;
 
 class VerificationButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      verifiedStyle: {backgroundColor: '#7AAAC3'}
+    }
+  }
+  componentDidMount() {
+    this.listenTo(Store, 'onAddVerification');
+  }
+  onAddVerification(params) {
+    if (params.action === 'addVerification') {
+      this.setState({verifiedStyle: {backgroundColor: '#E829F2'}});
+      this.props.navigator.pop();
+      Actions.messageList('', 'tradle.Message', params.resource);    
+    }
+  }
   render() {
     var resource = this.props.resource;
     var me = utils.getMe();
@@ -23,32 +44,36 @@ class VerificationButton extends Component {
     return resource.from  &&  fromId != meId
            ? <View style={styles.verification}>
                <TouchableHighlight underlayColor='transparent' onPress={this.verify.bind(this)}>
-                 <Icon name='ion|checkmark' size={30}  color='#ffffff'  style={styles.icon}/>
+                 <Icon name='ion|checkmark' size={30}  color='#ffffff'  style={[styles.icon, this.state.verifiedStyle]}/>
                </TouchableHighlight>
              </View>
            : <View></View>  
   }
   verify() {
-    this.props.navigator.pop();
-    // var resource = this.props.resource;
-    // var me = utils.getMe();
-    // var owner = this.props.resource[toClone];
-    // var verification = {
-    //   message: {
-    //     value: resource['_type'] + '_' + resource.rootHash;
-    //     title: utils.getModel(this.props.modelName).value.title;
-    //   }
-    //   owner: {
-    //     value: owner['_type'] + '_' + owner.rootHash;
-    //     title: owner.formatted
-    //   }
-    //   verifier: { 
-    //     value: me['_type'] + '_' : me.rootHash,
-    //     title: me.formatted
-    //   }
-    // }
+    // this.props.navigator.pop();
+    var resource = this.props.resource;
+    var me = utils.getMe();
+    var from = this.props.resource.from;
+    var model = utils.getModel(resource['_type']).value;
+    var verification = {
+      '_type': 'tradle.Verification',
+      document: {
+        id: resource['_type'] + '_' + resource.rootHash + '_' + resource.currentHash,
+        title: resource.message ? resource.message : model.title
+      },
+      from: {
+        id: from.id,
+        title: from.title
+      },
+      verifier: { 
+        id: me['_type'] + '_' + me.rootHash + '_' + me.currentHash,
+        title: me.formatted
+      }
+    }
+    Actions.addVerification(verification);
   }
 }
+reactMixin(VerificationButton.prototype, Reflux.ListenerMixin);
 
 var styles = StyleSheet.create({
   icon: {
@@ -56,7 +81,7 @@ var styles = StyleSheet.create({
     height: 40,
     borderWidth: 2,
     borderColor: '#D7E6ED',
-    backgroundColor: '#7AAAC3',
+    // backgroundColor: '#7AAAC3',
     borderRadius: 20,
   },
   verification: {
