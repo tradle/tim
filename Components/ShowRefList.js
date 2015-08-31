69bb6d0449193f1f3b8de833e720abcc91292fb9
pgmemk <ellen.katsnelson@lablz.com>
2015-08-31 13:20:01 +0100

    redesigning buttons on profile page

--- Components/ShowRefList.js
+++ Components/ShowRefList.js
@@ -3,8 +3,8 @@
 var React = require('react-native');
 var utils = require('../utils/utils');
 var ResourceList = require('./ResourceList');
-// var Icon = require('react-native-vector-icons/Ionicons');
-var { Icon } = require('react-native-icons');
+var Icon = require('react-native-vector-icons/Ionicons');
+// var { Icon } = require('react-native-icons');
 
 var buttonStyles = require('../styles/buttonStyles');
 var constants = require('tradle-constants');
@@ -18,7 +18,14 @@ var {
   TouchableHighlight,
   Component
 } = React;
-
+// <View style={[buttonStyles.container1]}>
+//                <TouchableHighlight onPress={this.createNewIdentity.bind(this)} underlayColor='transparent'>
+//                  <View>
+//                    <Icon name='plus'  size={25}  color='#f7f7f7'  style={buttonStyles.icon}/>
+//                    <Text style={[buttonStyles.text, {color: '#f7f7f7'}]}>Add Identity</Text>
+//                  </View>
+//                </TouchableHighlight>
+//              </View>
 class ShowRefList extends Component {
   constructor(props) {
     super(props);
@@ -28,36 +35,39 @@ class ShowRefList extends Component {
   }
   render() {
     var resource = this.props.resource;
-    var props = utils.getModel(resource[constants.TYPE]).value.properties;
+    var model = utils.getModel(resource[constants.TYPE]).value;
+    var props = model.properties;
 
     var refList = [];
-
+    var isIdentity = model.id === constants.TYPES.IDENTITY;
+    var isMe = isIdentity ? resource[constants.ROOT_HASH] === utils.getMe()[constants.ROOT_HASH] : true;
     // The profile page for the device owner has 2 more profile specific links: add new identity and switch identity
-    var pos = (utils.getMe()[constants.ROOT_HASH] === resource[constants.ROOT_HASH]) ? 80 : 0;
-    var marginTopStep = 5; //30;
     for (var p in props) {
+      if (isIdentity  &&  !isMe  &&  props[p].allowRoles  &&  props[p].allowRoles === 'me')
+        continue;
       if (p.charAt(0) === '_'  ||  !props[p].items  ||  !props[p].items.backlink)
         continue;
+      var icon = props[p].icon  ||  utils.getModel(props[p].items.ref).value.icon;
+      if (!icon)
+        icon = 'ios-checkmark-empty';
       refList.push(
-        <View style={{top: pos}}>
+        <View style={buttonStyles.container}>
            <TouchableHighlight onPress={this.showResources.bind(this, this.props.resource, props[p])} underlayColor='transparent'>
-           <View>
-             <View style={buttonStyles.buttonContent} />
-             <View style={{flexDirection: 'row', paddingHorizontal: 5}}>
-               <Icon name='ion|ios-paper-outline'  size={20}  color='#ffffff'  style={[buttonStyles.icon, {marginTop: -33}]}/>
-               <Text style={[buttonStyles.text, {marginTop: -30}]}>{props[p].title}</Text>
+             <View style={{alignItems: 'center'}}>
+               <Icon name={icon}  size={35}  color='#ffffff'  style={[buttonStyles.icon, {paddingLeft: 7}]}/>
+               <Text style={buttonStyles.text}>{props[p].title}</Text>
              </View>
-           </View>
            </TouchableHighlight>
          </View>
         );
-      pos += marginTopStep;
      }
-     return (
-        <View  style={[buttonStyles.container, {top: 15}]}>
-          {refList}
-        </View> 
-      );
+     return refList.length
+             ? (
+                <View  style={{flexDirection: 'row'}}>
+                  {refList}
+                </View> 
+              )
+             : null;
   }
 }
 reactMixin(ShowRefList.prototype, ResourceViewMixin);
--- Components/SwitchIdentity.js
+++ Components/SwitchIdentity.js
@@ -5,9 +5,10 @@ var utils = require('../utils/utils');
 var Reflux = require('reflux');
 var Actions = require('../Actions/Actions');
 var reactMixin = require('react-mixin');
-var { Icon } = require('react-native-icons');
 var buttonStyles = require('../styles/buttonStyles');
 var constants = require('tradle-constants');
+var Icon = require('react-native-vector-icons/Ionicons');
+
 
 var {
   View,
@@ -15,23 +16,19 @@ var {
   TouchableHighlight,
   Component
 } = React;
-
 class SwitchIdentity extends Component { 
   render() {
     var resource = this.props.resource;
     return resource[constants.ROOT_HASH] === utils.getMe()[constants.ROOT_HASH]
-           ? <View style={[buttonStyles.container, {top: 50}]}>
+           ? <View style={[buttonStyles.container]}>
                <TouchableHighlight onPress={this.showIdentities.bind(this)} underlayColor='transparent'>
-               <View>
-                 <View style={buttonStyles.buttonContent} />
-                 <View style={{flexDirection: 'row', paddingHorizontal: 5}}>                 
-                   <Icon name='fontawesome|arrows'  size={20}  color='#ffffff'  style={[buttonStyles.icon, {marginTop: -33}]}/>
-                   <Text style={[buttonStyles.text, {marginTop: -30}]}>Switch Identity</Text>
+                 <View style={{alignItems: 'center'}}>                 
+                   <Icon name='arrow-swap'  size={25}  color='#ffffff'  style={buttonStyles.icon}/>
+                   <Text style={buttonStyles.text}>Switch Identity</Text>
                  </View>
-               </View>
                </TouchableHighlight>
              </View>
-           : <View></View>;    
+           : null;    
   }
   showIdentities() {
     Actions.showIdentityList(utils.getMe());
