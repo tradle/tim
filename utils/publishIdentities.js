5b2281fab183bb8813abac423225dbf53e495e98
pgmemk <ellen.katsnelson@lablz.com>
2015-11-15 20:21:20 +0000

    publishOne is added

--- utils/publishIdentities.js
+++ utils/publishIdentities.js
@@ -1,5 +1,6 @@
 
 var Q = require('q')
+var typeforce = require('typeforce')
 var memdown = require('memdown')
 var Keeper = require('http-keeper')
 var Blockchain = require('cb-blockr')
@@ -10,13 +11,77 @@ var BASE_PORT = 22222
 
 module.exports = {
   one: publishOne,
-  all: publishAll
+  all: publishAll,
 }
 
 function publishOne (opts) {
+  var tim = newTim(opts)
+
+  var published = false
+  return tim.ready()
+    .then(loopUntilPublished)
+    .then(tim.destroy)
+    .then(function () {
+      console.log(tim.name(), tim.myCurrentHash())
+    })
+
+  function loopUntilPublished () {
+    if (published) return
+
+    return tim.identityPublishStatus()
+      .then(function (status) {
+        console.log(tim.name(), 'status', status)
+        published = status.current
+        if (published) return
+
+        if (!status.queued) {
+          return tim.publishMyIdentity()
+            .then(function () {
+              published = true
+            })
+        }
+
+        return waitForPublished()
+      })
+      .then(loopUntilPublished)
+  }
+
+  function waitForPublished () {
+    var defer = Q.defer()
+    var events = ['chained', 'unchained']
+    events.forEach(function (event) {
+      tim.on(event, checkIfPublished)
+    })
+
+    function checkIfPublished (info) {
+      tim.lookupObject(info)
+        .then(function (obj) {
+          if (obj[ROOT_HASH] !== tim.myCurrentHash()) return
+
+          events.forEach(function (e) {
+            tim.removeListener(e, checkIfPublished)
+          })
+
+          defer.resolve()
+        })
+    }
+
+    return defer.promise
+  }
+}
+
+function publishAll (arr) {
+  return arr.reduce(function (memo, next) {
+    return memo.then(function () {
+      return publishOne(next)
+    })
+  }, Q())
+}
+
+function newTim (opts) {
   typeforce({
-    identity: 'Object',
-    keys: 'Array'
+    identity: 'Identity',
+    keys: 'Array',
     afterBlockTimestamp: '?Number'
   }, opts)
 
@@ -41,52 +106,6 @@ function publishOne (opts) {
     afterBlockTimestamp: opts.afterBlockTimestamp || CONSTANTS.afterBlockTimestamp
   })
 
-  var published = false
-  return tim.ready()
-    .then(loopUntilPublished)
-    .then(tim.destroy)
-
-  function loopUntilPublished () {
-    return tim.identityPublishStatus()
-      .then(function (status) {
-        if (status.current) return // DONE!
-
-        if (!published && !status.queued) {
-          return tim.publishMyIdentity()
-            .then(function () {
-              published = true
-            })
-        }
-
-        return waitForPublished()
-      })
-      .then(loopUntilPublished)
-  }
-
-  function waitForPublished () {
-    var defer = Q.defer()
-    var events = ;['chained', 'unchained']
-    events.forEach(function (event) {
-      tim.on(event, checkIfPublished)
-    })
-
-    function checkIfPublished (info) {
-      tim.lookupObject(info)
-        .then(function (obj) {
-          if (obj[ROOT_HASH] !== tim.myCurrentHash()) return
-
-          events.forEach(function (e) {
-            tim.removeListener(e, checkIfPublished)
-          })
-
-          defer.resolve()
-        })
-    }
-
-    return defer.promise
-  }
-}
-
-function publishAll (arr) {
-  return Q.all(arr.map(publishOne))
+  tim.on('error', console.error)
+  return tim
 }
