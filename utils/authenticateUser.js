a4a03eff9867b745ab52d07cb6b05d65ed9184ec
Mark Vayngrib <mark@tradle.io>
2016-01-03 18:08:24 -0500

    fallback to passcode

--- utils/authenticateUser.js
+++ utils/authenticateUser.js
@@ -15,13 +15,6 @@ export async function authenticateUser () {
   if (authenticated) return authenticated
 
   try {
-    await TouchID.isSupported()
-  } catch (err) {
-    AlertIOS.alert(SETUP_MSG)
-    return false
-  }
-
-  try {
     await TouchID.authenticate('authenticate yourself!', true) // fall back to passcode
   } catch (err) {
     var message
