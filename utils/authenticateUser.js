2dd2ffd18cfed1144b48dc7b103b129237bd6719
Mark Vayngrib <mark@tradle.io>
2016-01-04 16:08:37 -0500

    rewrite authenticateUser in ES5 (es6 is a bitch to debug)

--- utils/authenticateUser.js
+++ utils/authenticateUser.js
@@ -1,44 +1,44 @@
 
 import { AlertIOS } from 'react-native'
 import TouchID from 'react-native-touch-id'
+import Q from 'q'
 
 var SETUP_MSG = 'Please set up Touch ID first, so the app can better protect your data.'
 var AUTH_FAILED_MSG = 'Authentication failed'
 var AUTHENTICATION_EXPIRES_IN = 60000
 var authenticated = false
 
-export async function unauthenticateUser () {
+export function unauthenticateUser () {
   authenticated = false
 }
 
-export async function authenticateUser () {
-  if (authenticated) return authenticated
+export function authenticateUser () {
+  if (authenticated) return Q(authenticated)
 
-  try {
-    await TouchID.authenticate('authenticate yourself!', true) // fall back to passcode
-  } catch (err) {
-    var message
-    switch (err.name) {
-      case 'LAErrorUserCancel':
-        break
-      case 'RCTTouchIDNotSupported':
-        // fall through
-      case 'LAErrorTouchIDNotAvailable':
-        throw new Error('device not supported')
-      case 'LAErrorTouchIDNotEnrolled':
-        message = SETUP_MSG
-        break
-      default:
-        message = AUTH_FAILED_MSG
-        break
-    }
+  return TouchID.authenticate('please unlock the app', true) // fall back to passcode
+    .then(() => {
+      setTimeout(unauthenticateUser, AUTHENTICATION_EXPIRES_IN)
+      return authenticated = true
+    })
+    .catch((err) => {
+      var message
+      switch (err.name) {
+        case 'LAErrorUserCancel':
+          break
+        case 'RCTTouchIDNotSupported':
+          // fall through
+        case 'LAErrorTouchIDNotAvailable':
+          throw new Error('device not supported')
+        case 'LAErrorTouchIDNotEnrolled':
+          message = SETUP_MSG
+          break
+        default:
+          message = AUTH_FAILED_MSG
+          break
+      }
 
-    if (message) AlertIOS.alert(message)
+      if (message) AlertIOS.alert(message)
 
-    return false
-  }
-
-  authenticated = true
-  setTimeout(unauthenticateUser, AUTHENTICATION_EXPIRES_IN)
-  return authenticated
+      return authenticated = false
+    })
 }
