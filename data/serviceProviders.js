a730d274a7970474a9f0726bacf24f789e56ff30
pgmemk <ellen.katsnelson@lablz.com>
2016-01-06 18:52:34 -0500

    specific to bank info moved here from environment setting

--- data/serviceProviders.js
+++ data/serviceProviders.js
@@ -1,50 +1,79 @@
 
 module.exports = {
-  lloyds: {
-    hash: '3a55cc6346fdd73a4ac4debd311d80cbaa53ebcd',
-    on: false
+  topLevelProvider: {
+    rabobank: [
+      "achmea",
+      "obvion",
+      "amstel",
+      "dll",
+      "rabobank",
+      "myorder"
+    ],
+    lloyds: ['lloyds'],
+    tradle: [
+      "digibank",
+      "relia",
+      "easy",
+      "safe",
+      "friendly",
+      "europi",
+      "people"
+    ]
   },
-  achmea: {
-    hash: '64e174085ef1ae52026e589c484d36a5c5f969aa',
-    on: false
-  },
-  obvion: {
-    hash: 'c83c53d07001dd95276b88af54e009e916f86f4b',
-    on: false
-  },
-  amstel: {
-    hash: '28b3bc9db174284f90abe775ef62cd8f974e8555',
-    on: false
-  },
-  dll: {
-    hash: 'd3a3c63d72c3288be9ddeffc69870a49188e2c11',
-    on: false
-  },
-  rabobank: {
-    hash: '7433dd127cd7e9dfd41e82162116fa480428d3f2'
-  },
-  myorder: {
-    hash: '707ae31e2a789593b68faf8331213b32da3ce4e0'
-  },
-  digibank: {
-    hash: '4ab4e9953c2a10fbc9bc884bd447b1003540995c',
-  },
-  relia: {
-    hash: '178cbc9f29c68728e56122d4981c34439dbf77cc',
-  },
-  easy: {
-    hash: '179d536d4fc033b0e074be8d756413302ea62805',
-  },
-  safe: {
-    hash: '3b31cbee623a1795fd6ecb6fe650cc2d874be958',
-  },
-  friendly: {
-    hash: '19b1bf07e11b921b0334e711caae9eedf6748af2',
-  },
-  europi: {
-    hash: 'd0b3f6780215cb8adfb9524810599b4f1f6444ae',
-  },
-  people: {
-    hash: '57bfdcacb61d2c0cbb0adb97f8eabced780e1d79',
-  },
+  providers: {
+    lloyds: {
+      hash: '3a55cc6346fdd73a4ac4debd311d80cbaa53ebcd',
+      on: false
+    },
+    achmea: {
+      hash: '64e174085ef1ae52026e589c484d36a5c5f969aa',
+      on: false
+    },
+    obvion: {
+      hash: 'c83c53d07001dd95276b88af54e009e916f86f4b',
+      on: false
+    },
+    amstel: {
+      hash: '28b3bc9db174284f90abe775ef62cd8f974e8555',
+      on: false
+    },
+    dll: {
+      hash: 'd3a3c63d72c3288be9ddeffc69870a49188e2c11',
+      on: false
+    },
+    rabobank: {
+      hash: '7433dd127cd7e9dfd41e82162116fa480428d3f2',
+    },
+    myorder: {
+      hash: '707ae31e2a789593b68faf8331213b32da3ce4e0',
+      title: 'My Order'
+    },
+    digibank: {
+      hash: '4ab4e9953c2a10fbc9bc884bd447b1003540995c',
+    },
+    relia: {
+      hash: '178cbc9f29c68728e56122d4981c34439dbf77cc',
+      title: 'Relia Bank'
+    },
+    easy: {
+      hash: '179d536d4fc033b0e074be8d756413302ea62805',
+      title: 'Easy Bank'
+    },
+    safe: {
+      hash: '3b31cbee623a1795fd6ecb6fe650cc2d874be958',
+      title: 'Safe Bank'
+    },
+    friendly: {
+      hash: '19b1bf07e11b921b0334e711caae9eedf6748af2',
+      title: 'Friendly Bank'
+    },
+    europi: {
+      hash: 'd0b3f6780215cb8adfb9524810599b4f1f6444ae',
+      title: 'Europi Bank'
+    },
+    people: {
+      hash: '57bfdcacb61d2c0cbb0adb97f8eabced780e1d79',
+      title: 'Prople Bank'
+    },
+  }
 }
