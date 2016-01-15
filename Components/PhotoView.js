d22049140a39816dcd7f181854d26b83e4b18ad2
pgmemk <ellen.katsnelson@lablz.com>
2016-01-15 16:53:14 -0500

    better defining size for main photo

--- Components/PhotoView.js
+++ Components/PhotoView.js
@@ -8,7 +8,6 @@ var PhotoCarousel = require('./PhotoCarousel')
 var reactMixin = require('react-mixin');
 var PhotoCarouselMixin = require('./PhotoCarouselMixin');
 var equal = require('deep-equal')
-var Device = require('react-native-device')
 var {
   StyleSheet,
   Image,
@@ -44,7 +43,6 @@ class PhotoView extends Component {
     var hasPhoto = resource.photos && resource.photos.length;
     var currentPhoto = this.state.currentPhoto || (hasPhoto  &&  resource.photos[0]);
     if (!currentPhoto) {
-      var icon;
       if (model.id === constants.TYPES.IDENTITY) {
         return (
           <View style={{height: 250, alignSelf: 'center', justifyContent: 'center'}}>
@@ -52,13 +50,8 @@ class PhotoView extends Component {
           </View>
         )
       }
-
-        // icon = 'ion|person';
       else
         return <View />
-        // return <View style={{height: 50}} />
-        // icon = 'ion|chatboxes'
-      // return <Icon name={icon} size={200}  color='#f6f6f4'  style={styles.icon} />
     }
 
     var url = currentPhoto.url;
@@ -67,31 +60,24 @@ class PhotoView extends Component {
     var source = uri.charAt(0) == '/' || uri.indexOf('data') === 0
                ? {uri: uri, isStatic: true}
                : {uri: uri}
-    // var style = (currentPhoto.width)
-    //           ? [styles.image, {height: currentPhoto.height * (currentPhoto.width/400)}]
-    //           : styles.image
-    // if (resource.photos.length == 1)
-    //   return <Image source={source} style={styles.image} />;
-    // else {
-      var nextPhoto;
-      var len = resource.photos.length;
-      for (var i=0; i<len  &&  !nextPhoto; i++) {
-        var p = resource.photos[i].url;
-        if (p === url)
-          nextPhoto = i === len - 1 ? resource.photos[0] : resource.photos[i + 1];
-      }
-      return <TouchableHighlight underlayColor='#ffffff' onPress={this.showCarousel.bind(this, resource.photos[0])}>
-                <Image source={source} style={styles.image} />
-              </TouchableHighlight>
-    // }
+    var nextPhoto;
+    var len = resource.photos.length;
+    for (var i=0; i<len  &&  !nextPhoto; i++) {
+      var p = resource.photos[i].url;
+      if (p === url)
+        nextPhoto = i === len - 1 ? resource.photos[0] : resource.photos[i + 1];
+    }
+    return <TouchableHighlight underlayColor='#ffffff' onPress={this.showCarousel.bind(this, {currentPhoto: resource.photos[0]})}>
+              <Image source={source} style={styles.image} />
+            </TouchableHighlight>
   }
 }
 reactMixin(PhotoView.prototype, PhotoCarouselMixin);
 
 var styles = StyleSheet.create({
   image: {
-    width: 400,
-    height: 350,
+    width: React.Dimensions.get('window').width,
+    height: React.Dimensions.get('window').height / 2,
     alignSelf: 'stretch'
   },
   icon: {
