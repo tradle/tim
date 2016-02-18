7b680e5fbd6e60ebee8666854647aefee75bb41b
Mark Vayngrib <mark@tradle.io>
2016-02-18 10:15:19 -0500

    edit reshrink

--- reshrink.sh
+++ reshrink.sh
@@ -3,4 +3,14 @@ if [ -f npm-shrinkwrap.json ]
     rm npm-shrinkwrap.json
 fi
 
-npm dedupe && npm prune && npm shrinkwrap && rn-nodeify --hack
+npm dedupe
+npm prune
+
+# hack until react-native supports multiple versions of same dep
+cd "node_modules/@tradle/utils/node_modules/create-torrent/node_modules/block-stream2"
+npm install --production
+cd $OLDPWD
+# end hack
+
+npm shrinkwrap
+rn-nodeify --hack
