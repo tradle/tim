5f1a38e590826cb6ab697c4feede096bd7892c9a
pgmemk <ellen.katsnelson@lablz.com>
2016-06-22 12:34:45 -0400

    android project

---
+++ styles/platformStyles.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import {StyleSheet, Platform} from 'react-native';
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#f7f7f7',
+    marginTop: Platform.OS === 'ios' ? 64 : 44,
+    flex: 1,
+  }
+})
