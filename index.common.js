f340e56af9a492ef7fe4d52b2e7c29d23e20a344
pgmemk <ellen.katsnelson@lablz.com>
2016-07-15 13:38:19 -0400

    code cleanup

--- index.common.js
+++ index.common.js
@@ -283,7 +283,7 @@ class TiMApp extends Component {
     if (ReactPerf) {
       setTimeout(function () {
         ReactPerf.stop()
-        console.log(ReactPerf.printWasted())
+        // console.log(ReactPerf.printWasted())
       }, 500)
     }
 
