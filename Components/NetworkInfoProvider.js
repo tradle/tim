a64cbbfc998f29358b2d96cba1077af5edb127fe
pgmemk <ellen.katsnelson@lablz.com>
2016-07-15 13:37:49 -0400

    code cleanup

--- Components/NetworkInfoProvider.js
+++ Components/NetworkInfoProvider.js
@@ -1,11 +1,5 @@
 'use strict'
 
-var reactMixin = require('react-mixin');
-var Store = require('../Store/Store');
-var Actions = require('../Actions/Actions');
-var Reflux = require('reflux');
-var ResourceList = require('./ResourceList');
-
 import {
   View,
   StyleSheet,
@@ -17,22 +11,8 @@ import React, { Component } from 'react'
 class NetworkInfoProvider extends Component {
   constructor(props) {
     super(props)
-    // this.state = {isConnected: this.props.connected}
   }
-  // componentDidMount() {
-  //   this.listenTo(Store, 'onChange')
-  // }
-
-  // onChange(params) {
-  //   if (params.action === 'connectivity')
-  //     this.setState({isConnected: params.isConnected})
-  // }
-
   render() {
-    // let pageView = <View/>
-    // if (this.props.component === 'ResourceList')
-    //   pageView = <ResourceList navigator={this.props.navigator} {...this.props.route.passProps}/>
-
     return this.props.connected
           ? <View/>
           : <View style={styles.bar}>
@@ -42,19 +22,14 @@ class NetworkInfoProvider extends Component {
   }
 }
 
-reactMixin(NetworkInfoProvider.prototype, Reflux.ListenerMixin);
-
 var styles = StyleSheet.create({
   bar: {
     backgroundColor: '#FF9B30',
-    // borderColor: '#FF9B30',
-    // borderWidth: 1,
-    // borderBottomColor: '#FF6D0D'
+    padding: 5,
   },
   text: {
     fontSize: 18,
     color: '#ffffff',
-    padding: 5,
     alignSelf: 'center'
   },
 });
