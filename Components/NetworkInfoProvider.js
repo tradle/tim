4cf33c901d4e39943d42f21bf17fb5ae54990d7e
pgmemk <ellen.katsnelson@lablz.com>
2016-07-21 17:40:53 -0400

    better displaying online/offline

--- Components/NetworkInfoProvider.js
+++ Components/NetworkInfoProvider.js
@@ -3,9 +3,13 @@
 import {
   View,
   StyleSheet,
-  Text
+  Text,
+  Alert,
+  TouchableOpacity
 } from 'react-native'
 
+import utils from '../utils/utils'
+var translate = utils.translate
 import React, { Component } from 'react'
 
 class NetworkInfoProvider extends Component {
@@ -13,10 +17,16 @@ class NetworkInfoProvider extends Component {
     super(props)
   }
   render() {
+    let dn = this.props.resource
+           ? translate('learnMoreDescriptionTo', utils.getDisplayName(this.props.resource))
+           : translate('learnMoreDescription')
     return this.props.connected
           ? <View/>
           : <View style={styles.bar}>
-              <Text style={styles.text}>no network</Text>
+              <Text style={styles.text}>{translate('noNetwork')}</Text>
+              <TouchableOpacity onPress={() => Alert.alert(translate('offlineMode'), dn, null)}>
+                <Text style={styles.text}>{translate('learnMore')}</Text>
+              </TouchableOpacity>
             </View>
 
   }
@@ -25,12 +35,15 @@ class NetworkInfoProvider extends Component {
 var styles = StyleSheet.create({
   bar: {
     backgroundColor: '#FF9B30',
-    padding: 5,
+    padding: 7,
+    flexDirection: "row",
+    justifyContent: "space-between"
   },
   text: {
     fontSize: 18,
     color: '#ffffff',
-    alignSelf: 'center'
+    alignSelf: 'center',
+    marginHorizontal: 10
   },
 });
 
