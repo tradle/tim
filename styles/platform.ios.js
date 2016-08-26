10f878dc5c3751ad441be05e88e876a15efa73ef
pgmemk <ellen.katsnelson@lablz.com>
2016-08-25 20:54:53 -0400

    using platform specific extension for styles

---
+++ styles/platform.ios.js
@@ -0,0 +1,62 @@
+'use strict';
+
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#f7f7f7',
+    marginTop: 64,
+    flex: 1,
+  },
+  navBarText: {
+    marginTop: 10,
+    fontSize: 17
+  },
+  // navBar: {
+  //   marginTop: 10,
+  //   padding: 3
+  // },
+  menuButtonNarrow: {
+    marginTop: -23,
+    paddingVertical: 5,
+    paddingHorizontal: 21,
+    height: 45,
+    borderRadius: 24,
+    // shadowOffset:{width: 5, height: 5},
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    shadowColor: '#afafaf',
+    backgroundColor: 'red'
+  },
+  menuButton: {
+    marginTop: -23,
+    paddingVertical: 5,
+    paddingHorizontal: 13,
+    borderRadius: 24,
+    // shadowOffset:{width: 5, height: 5},
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    shadowColor: '#afafaf',
+    backgroundColor: 'red'
+  },
+  menuButtonRegular: {
+    marginTop: -20,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 24,
+    // shadowOffset:{width: 5, height: 5},
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    shadowColor: '#afafaf',
+    backgroundColor: 'red'
+  },
+})
+var menuIcon = {
+  name: 'md-more',
+  color: '#ffffff'
+}
+exports.MenuIcon = menuIcon
+// Object.defineProperty(exports, 'MB', {
+//   icon: 'md-more',
+//   color: '#ffffff'
+// })
