69f247d045d231855ea075d0e96ac52ea2066d87
Mark Vayngrib <mark@tradle.io>
2016-09-02 13:04:39 -0400

    (web) clear history on refresh

--- index.web.js
+++ index.web.js
@@ -1,1 +1,9 @@
+// will not be necessary when we switch to redux and save all state including history
+// and restore from url
+if (window.history) {
+  if (history.length > 1) {
+    history.go(-(history.length - 2))
+  }
+}
+
 require('./index.common')
