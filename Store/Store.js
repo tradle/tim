1e674c7e72795d9d70dc1c51285293d55268d92a
Mark Vayngrib <mark@tradle.io>
2016-09-12 20:13:55 -0400

    re-enable keychain

--- Store/Store.js
+++ Store/Store.js
@@ -47,7 +47,7 @@ var welcome = require('../data/welcome.json');
 
 var sha = require('stable-sha1');
 var utils = require('../utils/utils');
-var Keychain = null //utils.isIOS() && require('../utils/keychain')
+var Keychain = utils.isIOS() && require('../utils/keychain')
 var translate = utils.translate
 var promisify = require('q-level');
 var asyncstorageDown = require('asyncstorage-down')
