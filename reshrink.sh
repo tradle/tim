618f17de22cbfe04c2575078f21e1b4487fde1fb
Mark Vayngrib <mark@tradle.io>
2016-09-24 21:14:16 -0400

    update sendy

--- reshrink.sh
+++ reshrink.sh
@@ -5,9 +5,4 @@ fi
 
 npm dedupe
 npm prune
-
-# hack until react-native supports multiple versions of same dep
-cd "node_modules/react-native-vector-icons/node_modules/yargs/node_modules/yargs-parser" && npm install camelcase && cd $OLDPWD
-# end hack
-
 npm shrinkwrap
