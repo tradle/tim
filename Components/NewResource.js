3ddae613634a4c96b9393a8648c5ebda415bee42
pgmemk <ellen.katsnelson@lablz.com>
2016-09-26 11:05:48 -0400

    change terms & conditions flow

--- Components/NewResource.js
+++ Components/NewResource.js
@@ -734,24 +734,17 @@ class NewResource extends Component {
     var button = this.state.isRegistration
                ? <View>
                    <TouchableHighlight style={styles.thumbButton}
-                        underlayColor='transparent' onPress={this.onSavePressed.bind(this)}>
+                        underlayColor='transparent' onPress={() => {
+                          if (this.state.termsAccepted)
+                            this.onSavePressed()
+                          else
+                            this.showTermsAndConditions()
+                        }}>
                       <View style={styles.getStarted}>
                          <Text style={styles.getStartedText}>ENTER</Text>
                       </View>
                    </TouchableHighlight>
-                   <TouchableHighlight underlayColor='transparent' style={{flexDirection: 'row', alignSelf: 'center', paddingTop: 7}} onPress={() => {
-                      this.props.navigator.push({
-                        id: 3,
-                        component: ResourceView,
-                        title: translate('termsAndConditions'),
-                        backButtonTitle: translate('back'),
-                        rightButtonTitle: translate('Accept'),
-                        passProps: {
-                          resource: termsAndConditions,
-                          action: this.acceptTsAndCs.bind(this)
-                        }
-                     })
-                   }}>
+                   <TouchableHighlight underlayColor='transparent' style={{flexDirection: 'row', alignSelf: 'center', paddingTop: 7}} onPress={this.showTermsAndConditions.bind(this)}>
                      <View style={{flexDirection: 'row'}}>
                        <Text style={{fontSize: 16, color: '#A6DBF5'}}>{translate('acceptTermsAndConditions')}</Text>
                      </View>
@@ -776,13 +769,12 @@ class NewResource extends Component {
             <View style={{marginTop: this.state.isRegistration ? 0 : -10}}>
               {arrayItems}
              </View>
-            {
-             this.state.isLoadingVideo
-             ? <View style={{alignItems: 'center', marginTop: 50}}>
-                <ActivityIndicator animating={true} size='large' color='#ffffff'/>
+              <View style={{alignItems: 'center', marginTop: 50}}>
+               {this.state.isLoadingVideo
+                    ? <ActivityIndicator animating={true} size='large' color='#ffffff'/>
+                    : <View/>
+                 }
               </View>
-             : <View/>
-            }
           </View>
         </View>
       </ScrollView>
@@ -814,18 +806,31 @@ class NewResource extends Component {
       </View>
     )
   }
+  showTermsAndConditions() {
+    this.props.navigator.push({
+      id: 3,
+      component: ResourceView,
+      title: translate('termsAndConditions'),
+      backButtonTitle: translate('back'),
+      rightButtonTitle: translate('Accept'),
+      passProps: {
+        resource: termsAndConditions,
+        action: this.acceptTsAndCs.bind(this)
+      }
+   })
+  }
   acceptTsAndCs() {
-    Alert.alert(
-      translate('acceptingTermsAndConditions'),
-      null,
-      [
-        {text: 'Cancel', onPress: () => console.log('Canceled!')},
-        {text: 'Ok', onPress: () => {
+    // Alert.alert(
+    //   translate('acceptingTermsAndConditions'),
+    //   null,
+    //   [
+    //     {text: 'Cancel', onPress: () => console.log('Canceled!')},
+    //     {text: 'Ok', onPress: () => {
           this.props.navigator.pop()
           this.setState({termsAccepted: true})
-        }},
-      ]
-    )
+    //     }},
+    //   ]
+    // )
   }
 
   cancelItem(pMeta, item) {
