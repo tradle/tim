275252c7afa3fe9009a3d564dc45f47a37a8ff88
Mark Vayngrib <mark@tradle.io>
2016-09-29 00:36:38 -0400

    prevent loop if history api is lying about history length

--- index.web.js
+++ index.web.js
@@ -1,12 +1,15 @@
 // will not be necessary when we switch to redux and save all state including history
 // and restore from url
-if (window.history) {
+if (window.history && !localStorage._tradleTimeTraveled) {
   if (history.length > 2) {
+    localStorage.setItem('_tradleTimeTraveled', 'y')
     history.go(-(history.length - 2))
   }
 }
 
+localStorage.removeItem('_tradleTimeTraveled')
 import './web/public/css/ionicons.css'
+// import './web/public/css/styles.css'
 require('./web/shims/deviceEventEmitter')
 require('./web/shims/orientation')
 require('./index.common')
