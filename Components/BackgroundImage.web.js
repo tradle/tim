e5c4b42c1a445d357fe7861660c0b1ed37a1b7c4
Mark Vayngrib <mark@tradle.io>
2016-09-29 22:11:50 -0400

    (web) prevent stretch in BackgroundImage

--- Components/BackgroundImage.web.js
+++ Components/BackgroundImage.web.js
@@ -3,7 +3,8 @@ import {
   Platform,
   StyleSheet,
   Image,
-  Dimensions
+  Dimensions,
+  View
 } from 'react-native'
 
 import { makeResponsive, getDimensions } from 'react-native-orient'
@@ -13,14 +14,16 @@ class BackgroundImage extends Component {
     const props = this.props
     const { width, height } = Dimensions.get('window')
     return (
-      <Image {...props} style={[
+      <View {...props} style={[
         props.style,
         {
+          background: `url(${props.source.uri || props.source}) no-repeat center center fixed`,
+          backgroundSize: 'cover',
+          width: '100%',
+          height: '100%',
           position: 'absolute',
           top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%'
+          left: 0
         }
       ]} />
     )
