1b41cb091c9afcc9dc7f8144e2ec79080029f4e0
pgmemk <ellen.katsnelson@lablz.com>
2016-10-05 17:32:47 -0400

    prettiee visuals

---
+++ Components/PageView.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import {
+  StyleSheet,
+  View,
+} from 'react-native'
+
+import React, { Component } from 'react'
+import utils from '../utils/utils'
+
+class PageView extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <View style={[this.props.style, styles.separator]}>
+        {this.props.children}
+      </View>
+    )
+  }
+}
+var styles = StyleSheet.create({
+  separator: {
+    borderColor: '#ffffff',
+    borderTopColor: '#cccccc',
+    borderWidth: StyleSheet.hairlineWidth
+  }
+})
+
+module.exports = PageView
