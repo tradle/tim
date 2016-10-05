ab4f5bc177ecca7a09a1d398f70bae1062a01c32
pgmemk <ellen.katsnelson@lablz.com>
2016-10-05 17:30:55 -0400

    using react-native-gallery instead of carousel

--- Components/PhotoCarousel.js
+++ Components/PhotoCarousel.js
@@ -17,26 +17,19 @@ class PhotoCarousel extends Component {
     var photoUrls = [];
     var currentPhoto = this.props.currentPhoto || this.props.photos[0];
     var currentPhotoIndex = -1;
-    var n = this.props.photos.length;
 
-    var r = this.props.resource
-    for (var j=0; j<2; j++) {
-      for (var i=0; i<n; i++) {
-        var photo = this.props.photos[i];
+    for (var i=0; i<this.props.photos.length; i++) {
+      var photo = this.props.photos[i];
 
-        if (currentPhotoIndex == -1  &&  photo.url !== currentPhoto.url)
-          continue;
-        photoUrls.push(photo.url)
-
-        if (currentPhotoIndex == -1)
-          currentPhotoIndex = i;
-      }
-      n = currentPhotoIndex
+      if (currentPhotoIndex === -1  &&  photo.url === currentPhoto.url)
+        currentPhotoIndex = i;
+      photoUrls.push(photo.url)
     }
     return (
       <Gallery
         style={{flex: 1, backgroundColor: 'black'}}
         images={photoUrls}
+        initialPage={currentPhotoIndex}
       />
     );
   }
