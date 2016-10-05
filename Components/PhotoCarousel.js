ee4fa298e5f2d39713a64ef7852003a66faefb72
pgmemk <ellen.katsnelson@lablz.com>
2016-10-05 13:44:05 -0400

    use react-native-gallery instead of carousel, allows pinch/spread and swipe

--- Components/PhotoCarousel.js
+++ Components/PhotoCarousel.js
@@ -1,15 +1,7 @@
 'use strict'
 
-var Carousel = require('react-native-carousel');
-var utils = require('../utils/utils');
-var constants = require('@tradle/constants');
-import { makeResponsive } from 'react-native-orient'
-
-import {
-  StyleSheet,
-  Image,
-  View,
-} from 'react-native'
+// import { makeResponsive } from 'react-native-orient'
+import Gallery from 'react-native-gallery'
 
 import React, { Component } from 'react'
 
@@ -22,119 +14,32 @@ class PhotoCarousel extends Component {
     return this.props.orientation !== nextProps.orientation
   }
   render() {
-    var photos = [];
+    var photoUrls = [];
     var currentPhoto = this.props.currentPhoto || this.props.photos[0];
     var currentPhotoIndex = -1;
     var n = this.props.photos.length;
 
-    var model = utils.getModel(this.props.resource[constants.TYPE]).value;
-    var isVertical = currentPhoto.isVertical
-    var isLicense = model.id.indexOf('License') !== -1  ||  model.id.indexOf('Passport') !== -1;
-    var isUtility = !isLicense  &&  model.id.indexOf('Utility') !== -1
-    var cnt = 2000
     var r = this.props.resource
-    var styles = createStyles()
     for (var j=0; j<2; j++) {
       for (var i=0; i<n; i++) {
         var photo = this.props.photos[i];
 
         if (currentPhotoIndex == -1  &&  photo.url !== currentPhoto.url)
           continue;
+        photoUrls.push(photo.url)
+
         if (currentPhotoIndex == -1)
           currentPhotoIndex = i;
-        var key = r[constants.ROOT_HASH] + cnt++
-        if (photo.width) {
-          var {width, height} = utils.dimensions(PhotoCarousel)
-          var w = width, h, padding
-          if (photo.width > width) {
-            h = photo.height * width / photo.width
-            // if (photo.isVertical)
-            //   h = Dimensions.get('window').width * 1.2
-            // else
-            //   h = Dimensions.get('window').width / 1.2
-          }
-          else {
-            h = photo.height
-            w = photo.width
-            padding = (width - photo.width) / 2
-          }
-
-          if (h > 3 * height / 4)
-            h = 3 * height / 4
-
-          photos.push(
-                        <View style={[styles.container, {paddingHorizontal: padding}]} key={key}>
-                          <Image resizeMode='cover' source={{uri: photo.url}} style={{width: w, height: h}}/>
-                        </View>
-                     )
-
-        }
-        else
-          photos.push(
-            isVertical  ? <View style={styles.container} key={key}>
-                            <Image resizeMode='cover' source={{uri: photo.url}} style={styles.imageV}/>
-                          </View>
-
-                        : (isLicense
-                           ? <View style={styles.container} key={key}>
-                                <Image resizeMode='cover' source={{uri: photo.url}} style={styles.imageH}/>
-                              </View>
-                           : <View style={styles.container} key={key}>
-                                <Image resizeMode='cover' source={{uri: photo.url}} style={styles.image}/>
-                              </View>
-                        )
-        )
-        // photos.push(
-        //   isLicense ? <View style={styles.container}>
-        //                <Image source={{uri: photo.url}} style={styles.imageH}/>
-        //               </View>
-        //             : (isUtility
-        //               ? <View style={styles.container}>
-        //                   <Image source={{uri: photo.url}} style={styles.imageV}/>
-        //                 </View>
-
-        //               : <View style={styles.container}>
-        //                   <Image source={{uri: photo.url}} style={styles.image}/>
-        //                 </View>
-        //               )
-        // )
       }
       n = currentPhotoIndex
     }
     return (
-      <Carousel width={width}
-                loop={false}
-                animate={false}
-                indicatorOffset={40}
-                indicatorColor={'#efefef'}>
-        {photos}
-      </Carousel>
+      <Gallery
+        style={{flex: 1, backgroundColor: 'black'}}
+        images={photoUrls}
+      />
     );
   }
 }
-PhotoCarousel = makeResponsive(PhotoCarousel)
-
-var createStyles = utils.styleFactory(PhotoCarousel, function ({ dimensions }) {
-  return StyleSheet.create({
-    image: {
-      width: dimensions.width,
-      height: dimensions.height - 200,
-    },
-    imageH: {
-      width: dimensions.width,
-      height: dimensions.width - 100
-    },
-    imageV: {
-      width: dimensions.width,
-      height: dimensions.height - 200,
-    },
-    container: {
-      width: dimensions.width,
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'black',
-    },
-  })
-})
+// PhotoCarousel = makeResponsive(PhotoCarousel)
 module.exports = PhotoCarousel;
