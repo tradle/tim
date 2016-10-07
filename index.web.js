e737e18d7c8cdb5e2d09e47f8faf78de96b6eddf
Mark Vayngrib <mark@tradle.io>
2016-10-07 16:21:23 -0400

    disable history clear trick

--- index.web.js
+++ index.web.js
@@ -1,13 +1,13 @@
 // will not be necessary when we switch to redux and save all state including history
 // and restore from url
-if (window.history && !localStorage._tradleTimeTraveled) {
-  if (history.length > 2) {
-    localStorage.setItem('_tradleTimeTraveled', 'y')
-    history.go(-(history.length - 2))
-  }
-}
+// if (window.history && !localStorage._tradleTimeTraveled) {
+//   if (history.length > 2) {
+//     localStorage.setItem('_tradleTimeTraveled', 'y')
+//     history.go(-(history.length - 2))
+//   }
+// }
 
-localStorage.removeItem('_tradleTimeTraveled')
+// localStorage.removeItem('_tradleTimeTraveled')
 import './css/customicons.css'
 import './css/ionicons.min.css'
 import './css/styles.css'
