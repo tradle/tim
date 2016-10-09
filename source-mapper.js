bffa05917badb12ddb1726cace5102b0811c29aa
Mark Vayngrib <mark@tradle.io>
2016-10-09 10:55:01 -0400

    accept /path/to/bundle in source-mapper.js

--- source-mapper.js
+++ source-mapper.js
@@ -6,19 +6,21 @@ var argv = require('minimist')(process.argv.slice(2), {
   alias: {
     h: 'help',
     r: 'release',
+    b: 'bundle',
     l: 'line',
     c: 'column'
   }
 })
 
-if (argv.help || !argv.release || isNaN(argv.line) || isNaN(argv.column)) {
+if (argv.help || !(argv.release || argv.bundle) || isNaN(argv.line) || isNaN(argv.column)) {
   printUsage()
   process.exit(1)
 }
 
 var SourceMapConsumer = require('source-map').SourceMapConsumer
-var minifiedJS = fs.readFileSync(path.resolve(argv.release, 'main.jsbundle'), 'utf8')
-var rawSourceMap = JSON.parse(fs.readFileSync(path.resolve(argv.release, 'main.jsbundle.map')))
+var bundlePath = argv.bundle ? path.resolve(argv.bundle) : path.resolve(argv.release, 'main.jsbundle')
+var minifiedJS = fs.readFileSync(bundlePath, 'utf8')
+var rawSourceMap = JSON.parse(fs.readFileSync(bundlePath + '.map'))
 rawSourceMap.sourcesContent = [minifiedJS]
 
 var smc = new SourceMapConsumer(rawSourceMap)
@@ -45,6 +47,7 @@ function printUsage () {
   Options:
       -h, --help              print usage
       -r, --release           release directory (should contain main.jsbundle and main.jsbundle.map)
+      -b, --bundle            path to js bundle file
       -l, --line              line number in minified source
       -c, --column            column number in minified source
   */
