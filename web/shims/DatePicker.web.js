0239abc65be0b0fbd86db836e0c8661f7d466c56
Mark Vayngrib <mark@tradle.io>
2016-10-13 13:00:17 -0400

    show year picker in datepicker

--- web/shims/DatePicker.web.js
+++ web/shims/DatePicker.web.js
@@ -13,12 +13,16 @@ import moment from 'moment'
 
 export default class DatePickerAdapter extends Component {
   render() {
-    return <DatePicker
-      dateFormat={this.props.format}
-      selected={this.props.date && moment(this.props.date)}
-      placeholderText={this.props.placeholder}
-      style={this.props.style}
-      onChange={this.props.onDateChange}
-    />
+    return (
+      <DatePicker
+        showYearDropdown
+        selected={this.props.date && moment(this.props.date)}
+        placeholderText={this.props.placeholder}
+        style={this.props.style}
+        onChange={this.props.onDateChange}
+      />
+    )
   }
 }
+
+        // dateFormat={this.props.format}
