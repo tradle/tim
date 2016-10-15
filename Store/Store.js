0ae07448ff576bcb768a8f21eb11ace2b4cfc507
Mark Vayngrib <mark@tradle.io>
2016-10-14 23:50:26 -0400

    send firstName in selfIntro or publishIdentityRequest

--- Store/Store.js
+++ Store/Store.js
@@ -1347,21 +1347,29 @@ var Store = Reflux.createStore({
     if (newContact) {
       if (data.name === '')
         data.name = data.identity.name && data.identity.name.formatted
-      if (isDevicePairing)
+      if (isDevicePairing) {
         profile = {
           [TYPE]: PROFILE,
           [ROOT_HASH]: hash,
           firstName:  me.firstName,
           formatted: me.formatted
         }
-
-      else
+      }
+      else {
         profile = {
           [TYPE]: PROFILE,
           [ROOT_HASH]: hash,
-          firstName:  data.name || data.message.split(' ')[0],
-          formatted: data.name || data.message.split(' ')[0]
+          ...data.profile
         }
+
+        if (!profile.firstName) {
+          profile.firstName = data.name || data.message.split(' ')[0]
+        }
+
+        if (!profile.formatted) {
+          profile.formatted = profile.firstName
+        }
+      }
 
       profile.formatted = profile.firstName + (data && data.lastName ? ' ' + data.lastName : '')
       var identity = data.identity
@@ -1600,7 +1608,9 @@ var Store = Reflux.createStore({
             message: me.firstName + ' is waiting for the response',
             [TYPE]: SELF_INTRODUCTION,
             identity: identity[0].publishedIdentity,
-            name: me.firstName,
+            profile: {
+              firstName: me.firstName
+            },
             from: me,
             to: r.to
           }
@@ -4117,7 +4127,9 @@ var Store = Reflux.createStore({
       [TYPE]: IDENTITY_PUBLISHING_REQUEST,
       [NONCE]: self.getNonce(),
       identity: meDriver.identity,
-      name: me.firstName
+      profile: {
+        firstName: me.firstName
+      }
     }
     var key = IDENTITY + '_' + orgRep[ROOT_HASH]
 
