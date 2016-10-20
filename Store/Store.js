8920981340830f874ae45be80a3446450100acf8
Mark Vayngrib <mark@tradle.io>
2016-10-20 16:05:20 -0400

    auto-restart app from onReloadDB

--- Store/Store.js
+++ Store/Store.js
@@ -2646,22 +2646,24 @@ var Store = Reflux.createStore({
     return destroyTim
       .then(() => this.wipe())
       .then(() => {
-        Alert.alert('please refresh')
-        return Q.Promise(function (resolve) {})
+        return utils.restartApp()
+        // Alert.alert('please refresh')
+        // return Q.Promise(function (resolve) {})
       })
-      .then(function() {
-        list = {};
-        models = {};
-        me = null;
-        return
-        // return self.loadModels()
-      })
-      .then(function() {
-        self.trigger({action: 'reloadDB', models: models});
-      })
-      .catch(function(err) {
-        err = err;
-      });
+      // .then(function() {
+      //   list = {};
+      //   models = {};
+      //   me = null;
+      //   return
+      //   // return self.loadModels()
+      // })
+      // .then(function() {
+      //   self.trigger({action: 'reloadDB', models: models});
+      // })
+      // .catch(function(err) {
+      //   err = err;
+      // });
+
     // var togo = 1;
     // // this.loadModels()
     // // var name = me.firstName.toLowerCase();
