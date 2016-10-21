5690371a32158e3bc4f6f6f5ed1c8a8d2dfdeb05
Mark Vayngrib <mark@tradle.io>
2016-10-21 17:19:52 -0400

    make android happy with platformUtils.platform naming

---
+++ utils/platformUtilsMobile.js
@@ -0,0 +1,37 @@
+
+import {
+  findNodeHandle,
+  NativeModules,
+  Platform
+} from 'react-native'
+
+import NavigationBarStylesIOS from 'NavigatorNavigationBarStylesIOS'
+import NavigationBarStylesAndroid from 'NavigatorNavigationBarStylesAndroid'
+
+const NavBarStyles = Platform.OS === 'ios' ? NavigationBarStylesIOS : NavigationBarStylesAndroid
+const RCTUIManager = NativeModules.UIManager
+
+module.exports = {
+  getNode: function getNode (component) {
+    return typeof component === 'number' ? component : findNodeHandle(component)
+  },
+  autoScroll: function (scrollView, toHandle, additionalOffset) {
+    // debugger
+    const scrollResponder = scrollView.getScrollResponder()
+    scrollResponder.scrollResponderScrollNativeHandleToKeyboard(
+      findNodeHandle(toHandle),
+      additionalOffset,
+      true
+    )
+  },
+  measure: function (scrollView, handle, cb) {
+    RCTUIManager.measureLayout(handle, findNodeHandle(scrollView.getInnerViewNode()), cb, function (left, top, width, height) {
+      cb(null, { left, top, width, height })
+    })
+  },
+  scrollTo: function (scrollView, x, y) {
+    const scrollResponder = scrollView.getScrollResponder()
+    scrollResponder.scrollResponderScrollTo({ x, y, animated: true});
+  },
+  navBarHeight: NavBarStyles.General.NavBarHeight
+}
