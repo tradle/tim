2097c8170c9eb55aaa9def4d11274149559b5d36
pgmemk <ellen.katsnelson@lablz.com>
2016-10-25 03:37:55 +0100

    context chooser clickable

--- Components/ChatContext.js
+++ Components/ChatContext.js
@@ -16,66 +16,62 @@ class ChatContext extends Component {
   constructor(props) {
     super(props)
   }
-  // render() {
-  //   return this.props.context
-  //         ? <View style={styles.bar}>
-  //             <Text style={styles.text}>{translate(utils.getModel(this.props.context.product).value)}</Text>
-  //             {this.props.allContexts
-  //               ? <TouchableOpacity onPress={this.props.contextChooser}>
-  //                   <Icon size={25} name='md-more' color='#AAAAAA' style={{marginRight: 10}} />
-  //                 </TouchableOpacity>
-  //               : <View/>
-  //             }
-  //           </View>
-  //         : <View/>
-
-  // }
 
   render() {
-    return this.props.context
-          ? <View style={this.props.allContexts ? styles.barAll : styles.bar}>
-              <TouchableOpacity onPress={this.props.contextChooser}>
-                <Text style={this.props.allContexts ? styles.textAll : styles.text}>{translate(utils.getModel(this.props.context.product).value)}</Text>
-              </TouchableOpacity>
+    if (!this.props.context)
+      return <View/>
+
+    return (
+            <TouchableOpacity onPress={this.props.allContexts ? this.props.contextChooser : this.props.shareWith}>
+              <View style={[this.props.allContexts ? styles.barAll : styles.barOne, styles.bar]}>
+                <Text style={[this.props.allContexts ? styles.textAll : styles.textOne, styles.text]}>{translate(utils.getModel(this.props.context.product).value)}</Text>
               {this.props.allContexts
-                ? <View />
-                : <TouchableOpacity onPress={this.props.shareWith}>
-                    <Icon size={22} name='md-share' color='#7D6EC4' style={{marginRight: 10, paddingLeft: 20}} />
-                  </TouchableOpacity>
+                ? <View/>
+                : <Icon size={22} name='md-share' color='#7D6EC4' style={{marginRight: 10, paddingLeft: 20}} />
               }
-            </View>
-          : <View/>
+              </View>
+            </TouchableOpacity>
+            )
+    // return this.props.context
+    //       ? <View style={this.props.allContexts ? styles.barAll : styles.bar}>
+    //           <TouchableOpacity onPress={this.props.contextChooser}>
+    //             <Text style={this.props.allContexts ? styles.textAll : styles.text}>{translate(utils.getModel(this.props.context.product).value)}</Text>
+    //           </TouchableOpacity>
+    //           {this.props.allContexts
+    //             ? <View />
+    //             : <TouchableOpacity onPress={this.props.shareWith}>
+    //                 <Icon size={22} name='md-share' color='#7D6EC4' style={{marginRight: 10, paddingLeft: 20}} />
+    //               </TouchableOpacity>
+    //           }
+    //         </View>
+    //       : <View/>
 
   }
 
 }
 
 var styles = StyleSheet.create({
-  bar: {
+  barOne: {
     backgroundColor: '#ECE8FF',
-    borderTopColor: '#dddddd',
-    borderTopWidth: StyleSheet.hairlineWidth,
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
   },
   barAll: {
     backgroundColor: '#f1ffe7',
+  },
+  bar: {
     borderTopColor: '#dddddd',
     borderTopWidth: StyleSheet.hairlineWidth,
     padding: 10,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   textAll: {
     fontSize: 18,
-    color: '#289427',
-    alignSelf: 'center',
-    marginHorizontal: 10
+  },
+  textOne: {
+    color: '#7D6EC4',
   },
   text: {
     fontSize: 18,
-    color: '#7D6EC4',
     alignSelf: 'center',
     marginHorizontal: 10
   }
