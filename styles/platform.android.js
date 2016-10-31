c144ca0e8803be0ea884d2fea0a5bbb98553bb71
pgmemk <ellen.katsnelson@lablz.com>
2016-10-31 11:47:02 +0000

    adjustable StyleSheet for fontSize settings calculated based on PixelRatio

--- styles/platform.android.js
+++ styles/platform.android.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import {StyleSheet, Platform} from 'react-native';
+import StyleSheet from '../StyleSheet'
+import {Platform} from 'react-native';
 import {getFontSize} from '../utils/utils'
 export default StyleSheet.create({
   container: {
