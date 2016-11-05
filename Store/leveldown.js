2352bca842749fd8eedab91b7e922b6363cf3b83
Mark Vayngrib <mark@tradle.io>
2016-11-05 14:40:36 -0400

    add indexeddb support in ie11

--- Store/leveldown.js
+++ Store/leveldown.js
@@ -9,23 +9,19 @@ var levelup = require('levelup')
 // var updown = require('level-updown')
 if (Platform.OS === 'web') {
   console.log('browser', browser)
-  if (browser.isIE) {
-    leveldown = require('localstorage-down')
-  } else {
-    var leveljs = require('level-js')
-    var dbs = []
-    leveldown = function () {
-      var result = leveljs.apply(this, arguments)
-      dbs.push(result)
-      return result
-    }
+  var leveljs = require('level-js')
+  var dbs = []
+  leveldown = function () {
+    var result = leveljs.apply(this, arguments)
+    dbs.push(result)
+    return result
+  }
 
-    leveldown.destroyAll = function () {
-      return Q.allSettled(dbs.map(db => Q.ninvoke(db, 'close')))
-        .then(() => {
-          return Q.all(dbs.map(db => Q.ninvoke(leveljs, 'destroy', db)))
-        })
-    }
+  leveldown.destroyAll = function () {
+    return Q.allSettled(dbs.map(db => Q.ninvoke(db, 'close')))
+      .then(() => {
+        return Q.all(dbs.map(db => Q.ninvoke(leveljs, 'destroy', db)))
+      })
   }
 }
 else {
