e7509e63965d93d2f7b238e234e73bbeb5cc36f9
pgmemk <ellen.katsnelson@lablz.com>
2016-11-10 18:36:51 -0500

    Employee onboarding productApp added to chat messages

--- Store/Store.js
+++ Store/Store.js
@@ -2322,6 +2322,7 @@ var Store = Reflux.createStore({
             msg.to = self.buildRef(orgRep)
             msg.from = self.buildRef(me)
             self._setItem(utils.getId(msg), msg)
+            self.addMessagesToChat(orgId, msg)
           })
           .catch(function (err) {
             debugger
