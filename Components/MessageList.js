109f2930870df41219d94bdd360bc7f5c9adedcf
Mark Vayngrib <mark@tradle.io>
2016-11-18 22:39:19 -0500

    rm show-actionsheet button if no options in actionsheet

--- Components/MessageList.js
+++ Components/MessageList.js
@@ -577,42 +577,53 @@ class MessageList extends Component {
       //   }
       // ]}
     let me = utils.getMe()
-    let buttons = []
-    if (this.state.isEmployee  &&  this.props.resource[constants.TYPE] !== constants.TYPES.ORGANIZATION)
-      buttons.push(translate('formChooser'))
-    else {
-      if (__DEV__) {
-        buttons.push(translate('forgetMe'))
-      }
-    }
-
-    buttons.push(translate('cancel'))
+    let actionSheet = this.renderActionSheet()
     return (
       <PageView style={[platformStyles.container, bgStyle]}>
         <NetworkInfoProvider connected={this.state.isConnected} resource={this.props.resource} />
         <ChatContext context={this.state.context} contextChooser={this.contextChooser.bind(this)} shareWith={this.shareWith.bind(this)} bankStyle={this.props.bankStyle} allContexts={this.state.allContexts} />
         <View style={ sepStyle } />
         {content}
-        <ActionSheet
-          ref={(o) => {
-            this.ActionSheet = o
-          }}
-          options={buttons}
-          cancelButtonIndex={1}
-          onPress={(index) => {
-            if (index === 0) {
-              if (this.state.isEmployee)
-                this.chooseFormForCustomer()
-              else
-                this.forgetMe()
-            }
-          }}
-        />
+        {actionSheet}
         {alert}
       </PageView>
     );
         // {addNew}
   }
+
+  renderActionSheet() {
+    let buttons = []
+    if (this.state.isEmployee  &&  this.props.resource[constants.TYPE] !== constants.TYPES.ORGANIZATION) {
+      buttons.push(translate('formChooser'))
+    }
+    else {
+      if (__DEV__) {
+        buttons.push(translate('forgetMe'))
+      }
+    }
+
+    if (!buttons.length) return
+
+    buttons.push(translate('cancel'))
+    return (
+      <ActionSheet
+        ref={(o) => {
+          this.ActionSheet = o
+        }}
+        options={buttons}
+        cancelButtonIndex={1}
+        onPress={(index) => {
+          if (index === 0) {
+            if (this.state.isEmployee)
+              this.chooseFormForCustomer()
+            else
+              this.forgetMe()
+          }
+        }}
+      />
+    )
+  }
+
   // Context chooser shows all the context of the particular chat.
   // When choosing the context chat will show only the messages in linked to this context.
   contextChooser(context) {
@@ -672,7 +683,7 @@ class MessageList extends Component {
     )
   }
   generateMenu(show) {
-    if (!show)
+    if (!show || !this.ActionSheet)
       return <View/>
     // {
     //   return <TouchableHighlight underlayColor='transparent' onPress={this.onSubmitEditing.bind(this)}>
