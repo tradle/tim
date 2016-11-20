4d32d5441bb13b448da4d0a604ab47d8ea9076e2
Mark Vayngrib <mark@tradle.io>
2016-11-20 10:34:26 -0500

    replace online status icons with circles in ResourceRow

---
+++ Components/Geometry.js
@@ -0,0 +1,37 @@
+
+import React, { Component, PropTypes } from 'react'
+import {
+  View
+} from 'react-native'
+
+class Circle extends Component {
+  static propTypes = {
+    size: PropTypes.number.isRequired,
+    style: View.propTypes.style        // style
+  };
+
+  render() {
+    return (
+      <View style={[
+        circleStyle(this.props.size),
+        this.props.style
+      ]}>
+        {this.props.children}
+      </View>
+    )
+  }
+}
+
+function circleStyle (size) {
+  return {
+    width: size,
+    height: size,
+    borderRadius: size / 2,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}
+
+module.exports = {
+  Circle
+}
