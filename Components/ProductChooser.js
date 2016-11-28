f57536300ce62005a5a6c3c4fee23ec9543bdcfd
pgmemk <ellen.katsnelson@lablz.com>
2016-11-27 22:34:07 -0500

    Shared context project: allow party context was shared with request corrections and filling out additional form

--- Components/ProductChooser.js
+++ Components/ProductChooser.js
@@ -55,7 +55,9 @@ class ProductChooser extends Component {
   }
   componentWillMount() {
     let r = this.props.resource
-    let id = r[constants.TYPE] === constants.TYPES.PROFILE ? utils.getId(utils.getMe().organization) : utils.getId(r)
+    let id = r[constants.TYPE] === constants.TYPES.PROFILE  ||  r[constants.TYPE] === PRODUCT_APPLICATION
+           ? utils.getId(utils.getMe().organization)
+           : utils.getId(r)
     Actions.getItem(id)
   }
   componentDidMount() {
@@ -65,8 +67,10 @@ class ProductChooser extends Component {
     let products = []
     if (params.action === 'getItem'  &&
         (this.props.resource[constants.ROOT_HASH] === params.resource[constants.ROOT_HASH] ||
-        this.props.resource[constants.TYPE] === constants.TYPES.PROFILE)) {
-      if (this.props.resource[constants.TYPE] === constants.TYPES.PROFILE) {
+        this.props.resource[constants.TYPE] === constants.TYPES.PROFILE)                   ||
+        this.props.resource[constants.TYPE] === PRODUCT_APPLICATION) {
+      if (this.props.resource[constants.TYPE] === constants.TYPES.PROFILE ||
+          this.props.resource[constants.TYPE] === PRODUCT_APPLICATION) {
         if (params.resource.products  &&  params.resource.products.length) {
           let context = this.props.context
           let productIds = context ? [context.product] : [params.resource.products]
