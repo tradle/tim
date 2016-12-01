12d7cb0969988a73f6a640c2d8b158737a660926
pgmemk <ellen.katsnelson@lablz.com>
2016-11-30 23:32:09 -0500

    better shared context arrow

--- Components/ResourceList.js
+++ Components/ResourceList.js
@@ -671,7 +671,7 @@ class ResourceList extends Component {
   openSharedContextChat(resource) {
     this.props.navigator.push({
       // title: translate(utils.getModel(resource.product).value) + ' -- ' + (resource.from.organization || resource.from.title) + ' ->  ' + resource.to.organization.title,
-      title: (resource.from.organization || resource.from.title) + '  ->  ' + resource.to.organization.title,
+      title: (resource.from.organization || resource.from.title) + '  →  ' + resource.to.organization.title,
       component: MessageList,
       id: 11,
       backButtonTitle: 'Back',
