e7ae4cd4cc67f0d48520794866a5592bd35325de
pgmemk <ellen.katsnelson@lablz.com>
2016-11-30 23:22:30 -0500

    show forms that relevant to current context or provider's products

--- Components/MessageList.js
+++ Components/MessageList.js
@@ -814,7 +814,7 @@ class MessageList extends Component {
         returnRoute: currentRoutes[currentRoutes.length - 1],
         callback: this.props.callback,
         type: constants.TYPES.FORM,
-        context: this.state.context
+        context: this.state.allContexts ? null : this.state.context
       },
       // rightButtonTitle: 'ion|plus',
       // onRightButtonPress: {
