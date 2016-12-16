96791051decb6beb027d7773973b35b3fb44bea5
pgmemk <ellen.katsnelson@lablz.com>
2016-12-15 21:20:00 -0500

    supervisory overview

---
+++ Components/StatisticsView.js
@@ -0,0 +1,300 @@
+'use strict';
+
+var PageView = require('./PageView')
+import ActionSheet from 'react-native-actionsheet'
+var utils = require('../utils/utils');
+var translate = utils.translate
+var reactMixin = require('react-mixin');
+var extend = require('extend')
+var Store = require('../Store/Store');
+var Actions = require('../Actions/Actions');
+var Reflux = require('reflux');
+var constants = require('@tradle/constants');
+var Icon = require('react-native-vector-icons/Ionicons');
+var buttonStyles = require('../styles/buttonStyles');
+var NetworkInfoProvider = require('./NetworkInfoProvider')
+var defaultBankStyle = require('../styles/bankStyle.json')
+var StyleSheet = require('../StyleSheet')
+
+import {Column as Col, Row} from 'react-native-flexbox-grid'
+
+const PRODUCT_APPLICATION = 'tradle.ProductApplication'
+// var bankStyles = require('../styles/bankStyles')
+
+import React, { Component, PropTypes } from 'react'
+import {
+  ListView,
+  // StyleSheet,
+  Navigator,
+  Alert,
+  // AlertIOS,
+  // ActionSheetIOS,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+  View,
+  Text,
+  Platform
+} from 'react-native';
+
+import platformStyles from '../styles/platform'
+import ENV from '../utils/env'
+
+const SearchBar = Platform.OS === 'android' ? null : require('react-native-search-bar')
+
+class StatisticsView extends Component {
+  props: {
+    navigator: PropTypes.object.isRequired,
+    resource: PropTypes.object.isRequired,
+  };
+  constructor(props) {
+    super(props);
+
+    let provider = props.provider
+    let applicants = Object.values(props.applicants)
+
+    let dataSource = new ListView.DataSource({
+        rowHasChanged: function(row1, row2) {
+          return row1 !== row2
+        }
+      })
+    this.state = {
+      // isLoading: utils.getModels() ? false : true,
+      // isLoading: true,
+      dataSource: dataSource.cloneWithRows(applicants),
+      isConnected: this.props.navigator.isConnected,
+    };
+  }
+  // componentWillMount() {
+  //   utils.onNextTransitionEnd(this.props.navigator, () => {
+  //     Actions.getAllPartials(this.props.resource, this.props.chat)
+  //   });
+  // }
+
+  componentDidMount() {
+    this.listenTo(Store, 'onStats');
+  }
+
+  onStats(params) {
+    var action = params.action;
+    if (action !== 'allPartials')
+      return
+    let applicants = Object.values(params.owners[this.props.provider.provider.id])
+
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(applicants),
+    })
+  }
+
+  selectResource(resource) {
+    var me = utils.getMe();
+    // Case when resource is a model. In this case the form for creating a new resource of this type will be displayed
+    var model = utils.getModel(this.props.modelName);
+    var m = utils.getModel(resource[constants.TYPE]).value;
+
+    var title = utils.makeTitle(utils.getDisplayName(resource, m.properties))
+    this.props.navigator.push({
+      title: title,
+      id: 3,
+      component: ResourceView,
+      backButtonTitle: 'Back',
+      passProps: {resource: resource}
+    });
+  }
+
+  renderRow(resource)  {
+    let app = resource.applications ? resource.applications[0] : null
+    let completionDate = app  &&  resource.completedApps[app.resource.title]
+    completionDate = completionDate ? utils.formatDate(new Date(completionDate)) : ''
+
+    return  <Row size={8} style={{borderBottomColor: '#aaaaaa', borderBottomWidth: 1}}>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={{alignSelf: 'center', padding: 3}}>
+                  {resource.owner.title || ''}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  {resource.forms.length}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  {resource.formErrors.length}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  {resource.formCorrections.length}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  {resource.verifications.length}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  {app && utils.formatDate(new Date(app.time)) || ''}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  {completionDate}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                </Text>
+              </Col>
+            </Row>
+
+  }
+  renderFooter() {
+    return <View/>
+  }
+  render() {
+    if (this.state.isLoading)
+      return <View/>
+    var content = <ListView
+          dataSource={this.state.dataSource}
+          renderHeader={this.renderHeader.bind(this)}
+          enableEmptySections={true}
+          renderRow={this.renderRow.bind(this)}
+          automaticallyAdjustContentInsets={false}
+          removeClippedSubviews={false}
+          keyboardDismissMode='on-drag'
+          keyboardShouldPersistTaps={true}
+          initialListSize={10}
+          pageSize={20}
+          scrollRenderAhead={10}
+          showsVerticalScrollIndicator={false} />
+
+    return (
+      <PageView style={platformStyles.container}>
+        <NetworkInfoProvider connected={this.state.isConnected} />
+        <View style={styles.separator} />
+        {content}
+      </PageView>
+    );
+  }
+  renderHeader() {
+    return <View style={{backgroundColor: '#f1ffe7'}}>
+            <Row size={8} style={styles.topRow}>
+              <Col sm={1} md={1} lg={1}>
+                <Text>
+                </Text>
+              </Col>
+              <Col sm={1} md={1} lg={1}>
+                <Text style={styles.topRowCell}>
+                  Forms
+                </Text>
+              </Col>
+              <Col sm={2} md={2} lg={2}>
+                <Text style={styles.topRowCell}>
+                  Corrections
+                </Text>
+              </Col>
+              <Col sm={1} md={1} lg={1}>
+                <Text style={styles.topRowCell}>
+                  Verified
+                </Text>
+              </Col>
+              <Col sm={2} md={2} lg={2}>
+                <Text style={styles.topRowCell}>
+                  Submissions
+                </Text>
+              </Col>
+              <Col sm={1} md={1} lg={1}>
+                <Text style={styles.topRowCell}>
+                  Elapsed
+                </Text>
+              </Col>
+            </Row>
+            <Row size={8} style={{borderBottomColor: '#aaaaaa', borderBottomWidth: 1}}>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={{fontWeight: '600', alignSelf: 'center', paddingVertical: 5 }}>
+                  {this.props.provider.title}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Submitted
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Requested
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Completed
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  {this.props.provider.title}
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Started
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Completed
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Days
+                </Text>
+              </Col>
+            </Row>
+          </View>
+
+
+    // let partial = <View style={{padding: 5, backgroundColor: '#f1ffe7'}}>
+    //                 <View style={styles.row}>
+    //                   <Icon name='ios-stats-outline' size={utils.getFontSize(45)} color='#246624' style={[styles.cellImage, {paddingLeft: 5}]} />
+    //                   <View style={styles.textContainer}>
+    //                     <Text style={styles.resourceTitle}>{translate('Statistics')}</Text>
+    //                   </View>
+    //                   <View style={styles.sharedContext}>
+    //                     <Text style={styles.sharedContextText}>{this.state.partialsCount}</Text>
+    //                   </View>
+    //                 </View>
+    //               </View>
+
+    return partial
+  }
+}
+reactMixin(StatisticsView.prototype, Reflux.ListenerMixin);
+
+var styles = StyleSheet.create({
+  topRow: {
+    borderBottomColor: '#aaaaaa',
+    borderBottomWidth: 1,
+  },
+  topRowCell: {
+    paddingVertical: 5,
+    fontSize:  16,
+    fontWeight: '600',
+    alignSelf: 'center',
+    borderRightColor: '#aaaaaa',
+    borderRightWidth: 1,
+  },
+  col: {
+    borderRightColor: '#aaaaaa',
+    borderRightWidth: 1,
+  },
+  cell: {
+    paddingVertical: 5,
+    fontSize: 14,
+    alignSelf: 'center'
+  },
+});
+
+module.exports = StatisticsView
---
+++ Components/SupervisoryView.js
@@ -0,0 +1,214 @@
+'use strict';
+
+var PageView = require('./PageView')
+import ActionSheet from 'react-native-actionsheet'
+var utils = require('../utils/utils');
+var translate = utils.translate
+var reactMixin = require('react-mixin');
+var extend = require('extend')
+var Store = require('../Store/Store');
+var Actions = require('../Actions/Actions');
+var Reflux = require('reflux');
+var constants = require('@tradle/constants');
+var Icon = require('react-native-vector-icons/Ionicons');
+var buttonStyles = require('../styles/buttonStyles');
+var NetworkInfoProvider = require('./NetworkInfoProvider')
+var StatisticsView = require('./StatisticsView')
+var defaultBankStyle = require('../styles/bankStyle.json')
+var StyleSheet = require('../StyleSheet')
+
+import {Column as Col, Row} from 'react-native-flexbox-grid'
+
+const PRODUCT_APPLICATION = 'tradle.ProductApplication'
+// var bankStyles = require('../styles/bankStyles')
+
+import React, { Component, PropTypes } from 'react'
+import {
+  ListView,
+  // StyleSheet,
+  Navigator,
+  Alert,
+  // AlertIOS,
+  // ActionSheetIOS,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+  View,
+  Text,
+  Platform
+} from 'react-native';
+
+import platformStyles from '../styles/platform'
+import ENV from '../utils/env'
+
+class SupervisoryView extends Component {
+  props: {
+    navigator: PropTypes.object.isRequired,
+    resource: PropTypes.object.isRequired,
+  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // isLoading: utils.getModels() ? false : true,
+      isLoading: true,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: function(row1, row2) {
+          return row1 !== row2 || row1._online !== row2._online
+        }
+      }),
+      isConnected: this.props.navigator.isConnected,
+    };
+  }
+  componentWillMount() {
+    utils.onNextTransitionEnd(this.props.navigator, () => {
+      Actions.getAllPartials(this.props.resource, this.props.chat)
+    });
+  }
+
+  componentDidMount() {
+    this.listenTo(Store, 'onStats');
+  }
+
+  onStats(params) {
+    var action = params.action;
+    if (action !== 'allPartials')
+      return
+    let list = params.stats
+
+    this.product = list[0].applications[0].leaves.filter((prop) => prop.key === 'product')[0].value
+
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(list),
+      list: list,
+      owners: params.owners,
+      isLoading: false
+    })
+  }
+
+  renderRow(resource)  {
+    let completed = resource.completedApps[this.product]
+    let open = resource.openApps[this.product]
+    return  <Row size={3} style={{borderBottomColor: '#aaaaaa', borderBottomWidth: 1}}>
+              <Col sm={1} md={1} lg={1} style={styles.col}>
+                <TouchableOpacity onPress={() => this.showProviderDetails(resource)}>
+                  <Text style={styles.topRowCell}>
+                    {resource.provider.title}
+                  </Text>
+                </TouchableOpacity>
+              </Col>
+              <Col sm={1} md={1} lg={1} style={styles.col}>
+                <Text style={styles.cell}>
+                  {completed || 0}
+                </Text>
+              </Col>
+              <Col sm={1} md={1} lg={1} style={styles.col}>
+                <Text style={styles.cell}>
+                  {open || 0}
+                </Text>
+              </Col>
+            </Row>
+
+  }
+  showProviderDetails(resource) {
+    // Actions.getAllPartials()
+    this.props.navigator.push({
+      id: 26,
+      component: StatisticsView,
+      backButtonTitle: 'Back',
+      title: resource.provider.title,
+      passProps: {
+        provider: resource,
+        applicants: this.state.owners[resource.provider.id]
+      }
+    })
+  }
+  render() {
+    if (this.state.isLoading)
+      return <View/>
+    var content = <ListView
+          dataSource={this.state.dataSource}
+          renderHeader={this.renderHeader.bind(this)}
+          enableEmptySections={true}
+          renderRow={this.renderRow.bind(this)}
+          automaticallyAdjustContentInsets={false}
+          removeClippedSubviews={false}
+          keyboardDismissMode='on-drag'
+          keyboardShouldPersistTaps={true}
+          initialListSize={10}
+          pageSize={20}
+          scrollRenderAhead={10}
+          showsVerticalScrollIndicator={false} />
+
+    return (
+      <PageView style={platformStyles.container}>
+        <NetworkInfoProvider connected={this.state.isConnected} />
+        <View style={styles.separator} />
+        {content}
+      </PageView>
+    );
+  }
+  renderHeader() {
+    if (this.state.isLoading)
+      return <View/>
+    // HACK for now
+    let app = utils.getModel(this.product).value
+    return <View style={{backgroundColor: '#f1ffe7'}}>
+            <Row size={3} style={styles.topRow}>
+              <Col sm={3} md={3} lg={3}>
+                <Text style={{fontWeight: '600', alignSelf: 'center', paddingVertical: 5, fontSize: 16 }}>
+                  {app.title}
+                </Text>
+              </Col>
+            </Row>
+            <Row size={3} style={{borderBottomColor: '#aaaaaa', borderBottomWidth: 1}}>
+              <Col sm={1} md={1} lg={1} style={styles.col}>
+                <Text style={{fontWeight: '600', alignSelf: 'center', paddingVertical: 5 }}>
+                  Provider
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Completed
+                </Text>
+              </Col>
+              <Col sm={1} md={4} lg={3} style={styles.col}>
+                <Text style={styles.cell}>
+                  Open
+                </Text>
+              </Col>
+            </Row>
+          </View>
+  }
+}
+reactMixin(SupervisoryView.prototype, Reflux.ListenerMixin);
+
+var styles = StyleSheet.create({
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#f7f7f7',
+  //   // backgroundColor: 'white',
+  //   marginTop: Platform.OS === 'ios' ? 64 : 44,
+  // },
+  topRow: {
+    borderBottomColor: '#aaaaaa',
+    borderBottomWidth: 1,
+  },
+  topRowCell: {
+    fontSize:16,
+    fontWeight: '600',
+    alignSelf: 'center'
+  },
+  col: {
+    paddingVertical: 5,
+    borderRightColor: '#aaaaaa',
+    borderRightWidth: 1,
+  },
+  cell: {
+    paddingVertical: 5,
+    fontSize: 14,
+    alignSelf: 'center'
+  },
+});
+
+module.exports = SupervisoryView
