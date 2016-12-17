f64e6fd9a02e0dd1bdfd45c00586d2a9027677c9
Mark Vayngrib <mark@tradle.io>
2016-12-16 21:19:25 -0500

    smarter local ip detection

--- utils/env.js
+++ utils/env.js
@@ -3,13 +3,24 @@ import {
   Platform
 } from 'react-native'
 
-const LOCAL_IP = require('./localIP')
+import DeviceInfo from 'react-native-device-info'
+
+const MACHINE_LOCAL_IP = require('./localIP')
+const LOCAL_IP = (function () {
+  if (Platform.OS === 'android') return '10.0.2.2'
+
+  if (Platform.OS === 'web' || DeviceInfo.getModel() === 'Simulator') {
+    return 'localhost'
+  }
+
+  return MACHINE_LOCAL_IP
+})()
 
 module.exports = {
   GCM_SENDER_ID: '633104277721',
   serviceID: 'tradle',
   accessGroup: '94V7783F74.io.tradle.dev',
-  LOCAL_IP: Platform.OS === 'android' ? '10.0.2.2' : LOCAL_IP,
+  LOCAL_IP: LOCAL_IP,
   LOCAL_SERVER: `http://${LOCAL_IP}:44444`,
   pushServerURL: __DEV__ ? `http://${LOCAL_IP}:48284` : 'https://push1.tradle.io',
   isAndroid: function () {
