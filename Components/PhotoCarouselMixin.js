4c252bf64900499c4bfbcedfcb49f758a9a9a42f
pgmemk <ellen.katsnelson@lablz.com>
2016-12-25 18:14:16 -0500

    better displaying header for image gallery

--- Components/PhotoCarouselMixin.js
+++ Components/PhotoCarouselMixin.js
@@ -5,18 +5,19 @@ import {
   Navigator
 } from 'react-native'
 import utils from '../utils/utils'
+var TYPE = require('@tradle/constants').TYPE
 
 var PhotoCarouselMixin = {
   showCarousel(currentPhoto, isView) {
-    let backButtonTitle = isView
-                        ? utils.translate('backTo', utils.getDisplayName(this.props.resource))
-                        : this.props.chat
-                           ? utils.translate('backTo', utils.getDisplayName(this.props.chat)) : utils.translate('backToResults')
+    // let backButtonTitle = isView
+    //                     ? utils.translate('backTo', utils.getDisplayName(this.props.resource))
+    //                     : this.props.chat
+    //                        ? utils.translate('backTo', utils.getDisplayName(this.props.chat)) : utils.translate('backToResults')
     this.props.navigator.push({
       id: 14,
-      // title: 'Photos',
+      title: utils.translate(utils.getModel(this.props.resource[TYPE]).value),
       // noLeftButton: true,
-      backButtonTitle: backButtonTitle,
+      backButtonTitle: 'Back',
       component: PhotoCarousel,
       passProps: {
         currentPhoto: currentPhoto,
@@ -24,7 +25,7 @@ var PhotoCarouselMixin = {
         resource: this.props.resource
       },
       // rightButtonTitle: 'Done',
-      // titleTintColor: 'black',
+      titleTextColor: '#f7f7f7',
       sceneConfig: Navigator.SceneConfigs.FadeAndroid,
       tintColor: '#dddddd',
       onRightButtonPress: {
