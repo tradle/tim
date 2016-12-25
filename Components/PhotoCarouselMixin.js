9f48dffb1248a3f7d89517701dc1a0023db42451
pgmemk <ellen.katsnelson@lablz.com>
2016-12-25 18:19:15 -0500

    better displaying photos

--- Components/PhotoCarouselMixin.js
+++ Components/PhotoCarouselMixin.js
@@ -8,15 +8,15 @@ import utils from '../utils/utils'
 
 var PhotoCarouselMixin = {
   showCarousel(currentPhoto, isView) {
-    let backButtonTitle = isView
-                        ? utils.translate('backTo', utils.getDisplayName(this.props.resource))
-                        : this.props.chat
-                           ? utils.translate('backTo', utils.getDisplayName(this.props.chat)) : utils.translate('backToResults')
+    // let backButtonTitle = isView
+    //                     ? utils.translate('backTo', utils.getDisplayName(this.props.resource))
+    //                     : this.props.chat
+    //                        ? utils.translate('backTo', utils.getDisplayName(this.props.chat)) : utils.translate('backToResults')
     this.props.navigator.push({
       id: 14,
-      // title: 'Photos',
+      title: utils.translate(utils.getModel(this.props.resource[TYPE]).value),
       // noLeftButton: true,
-      backButtonTitle: backButtonTitle,
+      backButtonTitle: 'Back',
       component: PhotoCarousel,
       passProps: {
         currentPhoto: currentPhoto,
