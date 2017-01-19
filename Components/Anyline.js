a07b3c5fd27ed6c9fd573510ad8d16d3913f7561
Mark Vayngrib <mark@tradle.io>
2017-01-18 20:19:44 -0500

    factor out blinkid

---
+++ Components/Anyline.js
@@ -0,0 +1,138 @@
+
+import { NativeModules, Platform } from 'react-native'
+import { anyline } from '../environment.json'
+import Anyline from 'anyline-ocr-react-native-module'
+import deepExtend from 'deep-extend'
+
+// import { parse as parseMRZ } from 'mrz'
+
+// from example
+
+function createConfig (options={}) {
+  let config = {
+    license: anyline.licenseKey,
+    options: deepExtend({
+      captureResolution: '480',
+      cutout: {
+        style: 'rect',
+        alignment: 'top',
+        offset: {
+          x: 0,
+          y: 120,
+        },
+        width: 426,
+        ratioFromSize: {
+          // B7 format (passport dimensions)
+          width: 426,
+          height: 300
+        },
+        strokeWidth: 2,
+        cornerRadius: 4,
+        strokeColor: 'F21C0A',
+        outerColor: '000000',
+        outerAlpha: 0.3,
+      },
+      flash: {
+        mode: 'manual',
+        alignment: 'bottom_right',
+      },
+      beepOnResult: true,
+      vibrateOnResult: true,
+      blinkAnimationOnResult: true,
+      cancelOnResult: true,
+      reportingEnabled: true,
+      visualFeedback: {
+        style: 'rect',
+        strokeColor: 'F21C0A',
+        fillColor: 'F21C0A',
+        cornerRadius: 2,
+      }
+    }, options)
+  }
+
+  if (Platform.OS === 'ios') {
+    config.options.doneButton = { // iOS only. Android uses hardware back button.
+      title: 'Cancel',
+      type: 'rect', // fullwidth, rect
+      cornerRadius: 0,
+      //backgroundColor:#EEEEEE, // default clearcolor
+      textColor: 'FFFFFF',
+      textColorHighlighted: 'CCCCCC',
+      fontSize: 33,
+      fontName: 'HelveticaNeue',
+      positionXAlignment: 'center', // left,right,center - no affect on fullwidth
+      positionYAlignment: 'bottom', // top, center, bottom
+      offset: {
+        x: 0, // postive -> right
+        y: -88, // postive -> down
+      }
+    }
+  }
+
+  return JSON.stringify(config)
+}
+
+module.exports = exports = Anyline
+const setupScanViewWithConfigJson = Anyline.setupScanViewWithConfigJson.bind(Anyline)
+exports.setupScanViewWithConfigJson = function ({ config, type='MRZ' }) {
+  type = type.toUpperCase()
+  if (!config) config = exports.config[type]
+  if (!config) throw new Error('expected "config"')
+
+  return new Promise((resolve, reject) => {
+    if (typeof config === 'object') config = JSON.stringify(config)
+
+    setupScanViewWithConfigJson(config, type, function (result) {
+      if (typeof result === 'string') result = JSON.parse(result)
+
+      if (type === 'MRZ') {
+        if (!result.allCheckDigitsValid) {
+          throw new Error('invalid MRZ')
+        }
+
+        // resolve({
+
+        // })
+      }
+
+      resolve(result)
+    }, reject)
+  })
+}
+
+exports.config = {
+  ocr: createConfig({
+    options: {
+      width: 400,
+      ratioFromSize: {
+        width: 1.3,
+        height: 1
+      }
+    }
+  }),
+  mrz: createConfig({
+    options: {
+      width: 400,
+      ratioFromSize: {
+        width: 1.42,
+        height: 1
+      }
+    }
+  }),
+  barcode: createConfig({
+    options: {
+      width: 400,
+      ratioFromSize: {
+        width: 1.3,
+        height: 1
+      }
+    }
+  })
+}
+
+// alias by built-in types
+exports.config.ANYLINE_OCR = exports.config.ocr
+exports.config.MRZ = exports.config.mrz
+exports.config.BARCODE = exports.config.barcode
+
+// console.log('ANYLINE', Anyline)
