8be05842b7ff797908a839df949b3315f0b8e8ae
Mark Vayngrib <mark@tradle.io>
2017-01-25 01:28:03 -0500

    don't show my qrcode more now

--- utils/env.js
+++ utils/env.js
@@ -41,5 +41,5 @@ module.exports = {
     return __DEV__
   },
   serverToSendLog: __DEV__ ? `http://${LOCAL_IP}:44444/userlog` : 'https://azure1.tradle.io/userlog',
-  showMyQRCode: true
+  showMyQRCode: false
 }
