1c4aac8ce5ed850afd93fed2cf846fd9e920dc26
pgmemk <ellen.katsnelson@lablz.com>
2017-01-28 23:34:42 -0500

    if remediation first this that opens chat PL is not send and product list shows empty

--- Components/ResourceList.js
+++ Components/ResourceList.js
@@ -1179,7 +1179,7 @@ class ResourceList extends Component {
           id: PROFILE + '_' + h[2]
         }
       }
-      Actions.addItem({resource: r, value: r, meta: utils.getModel('tradle.GuestSessionProof').value, disableAutoResponse: true})
+      Actions.addItem({resource: r, value: r, meta: utils.getModel('tradle.GuestSessionProof').value}) //, disableAutoResponse: true})
       break
     case TALK_TO_EMPLOYEEE:
       Actions.getEmployeeInfo(result.data.substring(h[0].length + 1))
