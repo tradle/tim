85d0319c84429498ab6ac880641fd83909a416f8
Mark Vayngrib <mark@tradle.io>
2017-02-05 10:45:46 -0500

    simplify web.sh

--- web.sh
+++ web.sh
@@ -5,7 +5,7 @@
 # npm i --save react@15.2.0 react-addons-perf@15.2.0 react-dom@15.2.0
 cp node_modules/tcomb-form-native/lib/templates/bootstrap/datepicker.ios.js node_modules/tcomb-form-native/lib/templates/bootstrap/datepicker.web.js
 cp node_modules/tcomb-form-native/lib/templates/bootstrap/select.ios.js node_modules/tcomb-form-native/lib/templates/bootstrap/select.web.js
-cp node_modules/react-native-local-auth/LocalAuth.android.js node_modules/react-native-local-auth/LocalAuth.web.js
+# cp node_modules/react-native-local-auth/LocalAuth.android.js node_modules/react-native-local-auth/LocalAuth.web.js
 cp node_modules/react-native-carousel/CarouselPager.android.js node_modules/react-native-carousel/CarouselPager.web.js
 sed -i '' "s/ReactLocalization;/ReactLocalization \|\| \{ language: 'en_US' \};/g" node_modules/react-native-localization/LocalizedStrings.js
 sed -i '' "s/RNDeviceInfo;/RNDeviceInfo \|\| \{};/g" node_modules/react-native-device-info/deviceinfo.js
