3e9bb6ea0108aef429c52ffa688ccbb68a21a8ba
pgmemk <ellen.katsnelson@lablz.com>
2017-02-10 11:24:51 -0500

    merge

--- Components/NewResourceMixin.js
+++ Components/NewResourceMixin.js
@@ -1173,7 +1173,7 @@ var NewResourceMixin = {
 
     let actionItem
     if (isVideo ||  isPhoto) {
-      if (prop.allowPicturesFromLibrary) {
+      if (isPhoto  &&  (params.prop !== 'scan'  ||  utils.isAndroid()  ||  utils.isWeb())) {
         var aiStyle = {flex: 7, paddingTop: 15, paddingBottom: 7}
         let m = utils.getModel(prop.ref).value
         actionItem = <ImageInput prop={prop} style={aiStyle} onImage={item => this.onSetMediaProperty(prop.name, item)}>
