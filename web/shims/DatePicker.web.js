251e134cbaf9d04a63f20939ee13055a47527214
Mark Vayngrib <mark@tradle.io>
2017-03-03 00:52:57 -0500

    Revert "use infinite calendar"
    This reverts commit e65c8c4359bfb52452efa59d06abfe850b0a7968.

--- web/shims/DatePicker.web.js
+++ web/shims/DatePicker.web.js
@@ -2,49 +2,28 @@
 
 import {
   View,
-  StyleSheet,
-  TouchableHighlight,
-  Text
+  StyleSheet
 } from 'react-native'
 
 import React, { Component } from 'react'
-// import DatePicker from 'react-datepicker/dist/react-datepicker'
-// require('react-datepicker/dist/react-datepicker.css')
+import DatePicker from 'react-datepicker/dist/react-datepicker'
+require('react-datepicker/dist/react-datepicker.css')
 
 import moment from 'moment'
-import dateformat from 'dateformat'
-import Actions from '../../Actions/Actions'
-import createCalendarModal from '../../Components/CalendarModal'
 
 export default class DatePickerAdapter extends Component {
-  onPress() {
-    const { onDateChange } = this.props
-    Actions.showModal({
-      contents: createCalendarModal({
-        selectedDate: this.props.date,
-        onConfirm: function (date) {
-          Actions.hideModal()
-          onDateChange(new Date(date))
-        },
-        onCancel: function () {
-          Actions.hideModal()
-        }
-      })
-    })
-  }
   render() {
-    const { date, customStyles } = this.props
-    const dateString = date ? dateformat(date, 'mmm dS, yyyy') : this.props.placeholder
-    const textStyle = customStyles && customStyles.placeholderText
     return (
-      <TouchableHighlight underlayColor="transparent" style={this.props.style} onPress={() => this.onPress()}>
-        <Text style={textStyle}>{dateString}</Text>
-      </TouchableHighlight>
+      <DatePicker
+        readOnly={true}
+        showYearDropdown
+        selected={this.props.date && moment(this.props.date)}
+        placeholderText={this.props.placeholder}
+        style={this.props.style}
+        onChange={this.props.onDateChange}
+      />
     )
   }
 }
 
-const styles = StyleSheet.create({
-  fix: {
-  }
-})
+        // dateFormat={this.props.format}
