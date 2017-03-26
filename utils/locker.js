3ed93c1633f9702832b67800e56b2375dd58032b
Mark Vayngrib <mark@tradle.io>
2017-03-25 22:10:41 -0400

    split out locker and crypto from utils

---
+++ utils/locker.js
@@ -0,0 +1,25 @@
+import mutexify from 'mutexify'
+
+module.exports = function (opts={}) {
+  const { timeout } = opts
+  const locks = new Map()
+  return function lockID (something) {
+    let lock = locks.get(something)
+    if (!lock) {
+      lock = mutexify()
+      locks.set(something, lock)
+    }
+
+    return new Promise(function (resolve, reject) {
+      lock(function (unlock) {
+        resolve(unlock)
+        if (!timeout) return
+
+        setTimeout(() => {
+          reject(new Error('timed out'))
+          unlock()
+        }, timeout)
+      })
+    })
+  }
+}
