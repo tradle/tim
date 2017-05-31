245eb58fb82f1306ad3214d45fb4c7481ee512e6
pgmemk <ellen.katsnelson@lablz.com>
2017-05-31 02:29:16 -0400

    default prop values handling on demand; test servers will have sandbox:true instead of title starting with [TEST]

--- Store/Store.js
+++ Store/Store.js
@@ -1843,6 +1843,8 @@ var Store = Reflux.createStore({
           if (sp.id !== id)
             return
         }
+        if (sp.org.name.indexOf('[TEST]') === 0)
+          return
         if (hash) {
           if (sp.bot[ROOT_HASH] !== hash)
             return
@@ -1927,8 +1929,10 @@ var Store = Reflux.createStore({
     else {
       let newOrg = {}
       extend(newOrg, sp.org)
-      if (newOrg.name.indexOf('[TEST]') === 0)
+      if (sp.sandbox === true)
         newOrg._isTest = true
+      // if (newOrg.name.indexOf('[TEST]') === 0)
+      //   newOrg._isTest = true
       this.configProvider(sp, newOrg)
       batch.push({type: 'put', key: okey, value: newOrg})
       this._setItem(okey, newOrg)
@@ -2071,23 +2075,24 @@ var Store = Reflux.createStore({
         this.dbPut(orgId, org)
       }
     }
-    if (org._defaultPropertyValues) {
-      for (let m in org._defaultPropertyValues) {
-        let mm = this.getModel(m)
-        let props = mm.properties
-        let mObj = org._defaultPropertyValues[m]
-        for (let p in mObj) {
-          if (props[p].ref  &&  this.getModel(props[p].ref).subClassOf === ENUM) {
-            let enumList = this.searchNotMessages({modelName: props[p].ref})
-            let eprop = utils.getEnumProperty(this.getModel(props[p].ref))
-            let val = enumList.filter((eVal) => eVal[eprop] === mObj[p])
-            if (val.length)
-              mObj[p] = this.buildRef(val[0])
-          }
-        }
-      }
-      utils.addDefaultPropertyValuesFor(org)
-    }
+    // if (org._defaultPropertyValues) {
+    //   debugger
+    //   for (let m in org._defaultPropertyValues) {
+    //     let mm = this.getModel(m)
+    //     let props = mm.properties
+    //     let mObj = org._defaultPropertyValues[m]
+    //     for (let p in mObj) {
+    //       if (props[p].ref  &&  this.getModel(props[p].ref).subClassOf === ENUM) {
+    //         let enumList = this.searchNotMessages({modelName: props[p].ref})
+    //         let eprop = utils.getEnumProperty(this.getModel(props[p].ref))
+    //         let val = enumList.filter((eVal) => eVal[eprop] === mObj[p])
+    //         if (val.length)
+    //           mObj[p] = this.buildRef(val[0])
+    //       }
+    //     }
+    //   }
+    //   utils.addDefaultPropertyValuesFor(org)
+    // }
     if (org._hidePropertyInEdit)
       utils.addHidePropertyInEditFor(org)
     if (config.greeting) {
@@ -3389,8 +3394,24 @@ var Store = Reflux.createStore({
       // Add items properties if they were created
       var json = utils.clone(value) // maybe not the best way to copy, try `clone`?
       for (p in resource) {
-        if (props[p]  &&  props[p].type === 'array')
+        if (!props[p])
+          continue
+        if (props[p].type === 'array')
           json[p] = resource[p];
+        let ref = props[p].ref
+        // Chaeck if valid enum value
+        if (ref  &&  this.getModel(ref).subClassOf === ENUM) {
+          if ((typeof json[p] === 'string')  ||  !this._getItem(utils.getId(json[p]))) {
+            let enumList = this.searchNotMessages({modelName: ref})
+            let eprop = utils.getEnumProperty(this.getModel(ref))
+            let val = enumList.filter((eVal) => eVal[eprop] === json[p])
+            if (val.length) {
+              if (resource[p] === json[p])
+                resource[p] = this.buildRef(val[0])
+              json[p] = this.buildRef(val[0])
+            }
+          }
+        }
       }
       if (!json[TYPE])
         json[TYPE] = meta.id;
@@ -5056,7 +5077,7 @@ var Store = Reflux.createStore({
     }
     function doFilterOut(r, toId, idx) {
       let m = self.getModel(r[TYPE])
-      if (m.id === PRODUCT_APPLICATION  &&  r.product === REMEDIATION)
+      if (m.id === PRODUCT_APPLICATION  &&  (r.product === REMEDIATION || !utils.getModel(r.product)))
         return true
       if (r._notSent)
         return true
@@ -5974,7 +5995,7 @@ var Store = Reflux.createStore({
         return
       dn = translate('sharedForm', translate(model), orgName)
       sharedWithOrg.lastMessage = dn
-      sharedWithOrg.lastMessageTime = value.time;
+      sharedWithOrg.lastMessageTime = value.time
       sharedWithOrg.lastMessageType = messageType
       batch.push({type: 'put', key: utils.getId(sharedWithOrg), value: sharedWithOrg});
       this.trigger({action: 'list', list: this.searchNotMessages({modelName: ORGANIZATION}), forceUpdate: true})
@@ -6000,8 +6021,17 @@ var Store = Reflux.createStore({
       let docType = utils.getId(value.document).split('_')[0]
       dn = translate('receivedVerification', translate(this.getModel(docType)))
     }
-    else if (model.id === PRODUCT_APPLICATION)
-      dn = this.getModel(value.product).title
+    else if (model.id === PRODUCT_APPLICATION) {
+      let m = this.getModel(value.product)
+      dn = m  &&  m.title
+      if (!dn) {
+        let s = value.product.split('.')
+          // insert a space before all caps
+        dn  = s[s.length - 1].replace(/([A-Z])/g, ' $1')
+          // uppercase the first character
+          .replace(/^./, function(str){ return str.toUpperCase(); }).trim()
+      }
+    }
     else if (model.subClassOf === MY_PRODUCT)
       dn = translate('receivedProduct', translate(model))
     else if (model.subClassOf === FORM) {
