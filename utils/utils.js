1a68c51d0ab288db64715ba059cc5e59904d24e1
pgmemk <ellen.katsnelson@lablz.com>
2017-07-27 11:43:22 -0400

    crashes when nothing to translate fix

--- utils/utils.js
+++ utils/utils.js
@@ -312,6 +312,8 @@ var utils = {
     return strMap[str]
   },
   createAndTranslate(s, isEnumValue) {
+    if (!s.length)
+      return null
     let stringName = s.replace(/\w\S*/g, function(txt) {
       return  isEnumValue
             ? txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
