3cfc5c80b8bf2aab1da959511690bd63dfb88269
pgmemk <ellen.katsnelson@lablz.com>
2017-07-28 19:41:22 -0400

    browsing, searching and sorting resources for employee mode using GraphQL

--- Store/Store.js
+++ Store/Store.js
@@ -564,7 +564,7 @@ var Store = Reflux.createStore({
         return self.readseal(value)
       case 'error':
         debugger
-        console.log(err)
+        console.log(data)
       }
     }      // worker: (event, cb) => {
   },
