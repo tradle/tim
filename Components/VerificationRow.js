a354bc36a56fb6b9667571585d31c2e7848bb18d
pgmemk <ellen.katsnelson@lablz.com>
2017-07-28 19:29:30 -0400

    employee browsing resources; highlighting the criteria

--- Components/VerificationRow.js
+++ Components/VerificationRow.js
@@ -18,7 +18,9 @@ var CURRENCY_SYMBOL
 const ITEM = 'tradle.Item'
 const MY_PRODUCT = 'tradle.MyProduct'
 const FORM = 'tradle.Form'
+const FORM_REQUEST = 'tradle.FormRequest'
 const ORGANIZATION = 'tradle.Organization'
+const PRODUCT_APPLICATION = 'tradle.ProductApplication'
 const PROFILE = constants.TYPES.PROFILE
 
 const TYPE = constants.TYPE
@@ -59,6 +61,8 @@ class VerificationRow extends Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.searchCriteria !== nextProps.searchCriteria)
+      return true
     return false
   }
 
@@ -69,6 +73,9 @@ class VerificationRow extends Component {
     var isForm = model.subClassOf === FORM
     var isVerification = resource.document != null
     var r = isVerification ? resource.document : resource
+
+    let listModel = utils.getModel(this.props.modelName).value
+    let ph = utils.getMainPhotoProperty(listModel)
 
     var photo
     if (r  &&  isMyProduct)
@@ -94,7 +101,7 @@ class VerificationRow extends Component {
       photo = <View style={{alignItems: 'center', width: 70}}>
                 <Icon name={model.icon || 'ios-ribbon-outline'} size={40} style={{marginTop: 8}} color={model.iconColor ? model.iconColor : '#cccccc'} />
               </View>
-    else
+    else if (ph)
       photo = <View style={{width: 70}} />
 
     var verificationRequest = resource.document
@@ -135,6 +142,10 @@ class VerificationRow extends Component {
                 : translate('verifiedByOn', title)
       verifiedBy = <View style={{alignItems: 'flex-end', paddingRight: 5}}><Text style={styles.verifiedBy}>{by}</Text></View>
     }
+    else if (this.props.search  &&  listModel.id === FORM_REQUEST) {
+      let by =  'Sent from ' + resource.from.organization.title
+      verifiedBy = <View style={{alignItems: 'flex-end', paddingRight: 5}}><Text style={styles.verifiedBy}>{by}</Text></View>
+    }
 
     let dateP = resource.dateVerified ? 'dateVerified' : resource.date ? 'date' : 'time'
     var date = r  &&  <View style={{alignItems: 'flex-end'}}>
@@ -144,8 +155,16 @@ class VerificationRow extends Component {
     let title
     if (isChooser  ||  model.interfaces.indexOf(ITEM) !== -1)
       title = dn //utils.getDisplayName(resource, model.properties)
-    if (!title || !title.length)
-      title = verificationRequest.title || utils.makeModelTitle(verificationRequest)
+    if (!title || !title.length) {
+      if (listModel.id === FORM_REQUEST)
+        title = utils.makeModelTitle(resource.form)
+      if (this.props.search) {
+        // if (listModel.id === PRODUCT_APPLICATION)
+          title = 'Submited by ' + resource.from.title
+      }
+      else
+        title = verificationRequest.title || utils.makeModelTitle(verificationRequest)
+    }
 
     let description = title === dn ? null : <Text style={styles.description}>{dn}</Text>
 
@@ -178,6 +197,9 @@ class VerificationRow extends Component {
           sharedFrom = <Text style={styles.verifiedBy}>Shared from {resource.to.organization.title}</Text>
       }
     }
+    let renderedRows = []
+    if (this.props.search  &&  this.props.searchCriteria)
+      this.formatFilteredResource(model, resource, renderedRows)
 
     var header =  <View style={[styles.header, {flex: 1}]} key={this.getNextKey()}>
                     <View style={{flexDirection: 'row', marginHorizontal: 10}}>
@@ -187,6 +209,7 @@ class VerificationRow extends Component {
                           <View style={{flexDirection: 'column', flex: 1}}>
                             {titleComponent}
                             {description}
+                          {renderedRows}
                           </View>
                           {supportingDocuments}
                         </View>
@@ -207,6 +230,9 @@ class VerificationRow extends Component {
               </TouchableHighlight>
             </View>
     else if (notAccordion) {
+      let renderedRows = []
+      if (this.props.search  &&  this.props.searchCriteria)
+        this.formatFilteredResource(model, resource, renderedRows)
       let content = <TouchableHighlight onPress={onSelect.bind(this)} underlayColor='transparent'>
                       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                         {header}
@@ -216,7 +242,6 @@ class VerificationRow extends Component {
         content = <Swipeout right={[{text: 'Revoke', backgroundColor: 'red', onPress: this.revokeDocument.bind(this)}]} autoClose={true} scroll={(event) => this._allowScroll(event)}>
                     {content}
                   </Swipeout>
-
       row = <View style={{backgroundColor: '#fff'}} host={lazy}>
              {content}
             </View>
@@ -265,6 +290,133 @@ class VerificationRow extends Component {
       passProps: {url: url}
     });
   }
+
+  formatFilteredResource(model, resource, renderedRows) {
+    var props = (utils.getModel(resource[TYPE] || resource.id).value).properties
+    let searchCriteria = this.props.searchCriteria
+    let viewCols = []
+    for (let p in searchCriteria) {
+      if (!props[p] || p.charAt(0) === '_')
+        continue
+      viewCols.push(p)
+    }
+
+    // var viewCols = model.gridCols || model.viewCols;
+    if (!viewCols)
+      return
+    var verPhoto;
+    var vCols = [];
+
+    var properties = model.properties;
+    var noMessage = !resource.message  ||  !resource.message.length;
+    var onPressCall;
+
+    var isSimpleMessage = model.id === constants.TYPES.SIMPLE_MESSAGE;
+    var style = styles.resourceTitle
+    var labelStyle = styles.resourceTitleL
+    viewCols.forEach((v) => {
+      if (properties[v].type === 'array'  ||  properties[v].type === 'date')
+        return;
+      if (!resource[v]  &&  !properties[v].displayAs)
+        return
+
+      var units = properties[v].units && properties[v].units.charAt(0) !== '['
+                ? ' (' + properties[v].units + ')'
+                : ''
+
+      if (properties[v].ref) {
+        if (resource[v]) {
+          let val
+          if (properties[v].ref === constants.TYPES.MONEY)
+            val = utils.normalizeCurrencySymbol(resource[v].currency || CURRENCY_SYMBOL) + resource[v].value
+          else if (resource[v].title)
+            val = resource[v].title
+          else
+            return
+
+          vCols.push(
+            <View style={styles.refPropertyRow} key={this.getNextKey()}>
+              <Text style={labelStyle}>{properties[v].title + units}</Text>
+              <Text style={[style, {fontWeight: '600'}]}>{val}</Text>
+            </View>
+          );
+        }
+
+        return;
+      }
+      let row
+      if (resource[v]  &&  properties[v].type === 'string'  &&  (resource[v].indexOf('http://') == 0  ||  resource[v].indexOf('https://') == 0))
+        row = <Text style={style} key={this.getNextKey()}>{resource[v]}</Text>;
+      else if (!model.autoCreate) {
+        var val = (properties[v].displayAs)
+                ? utils.templateIt(properties[v], resource)
+                : properties[v].type === 'object' ? null : resource[v];
+        if (!val)
+          return
+        let criteria = searchCriteria[v]
+
+        let row
+        if (criteria === val || properties[v].type !== 'string')
+          row = <Text style={[style, {fontWeight: '600'}]} key={this.getNextKey()}>{val}</Text>
+        else {
+          criteria = criteria.replace('*', '')
+          let idx = val.indexOf(criteria)
+          let part
+          let parts = []
+          if (idx > 0) {
+            parts.push(<Text style={style} key={this.getNextKey()}>{val.substring(0, idx)}</Text>)
+            idx++
+          }
+          parts.push(<Text style={[style, {fontWeight: '800'}]} key={this.getNextKey()}>{val.substring(idx, idx + criteria.length)}</Text>)
+          idx += criteria.length
+          if (idx < val.length)
+            parts.push(<Text style={style} key={this.getNextKey()}>{val.substring(idx)}</Text>)
+          row = <Text key={this.getNextKey()}>
+                  {parts}
+                </Text>
+        }
+        vCols.push(
+          <View style={styles.refPropertyRow} key={this.getNextKey()}>
+            <Text style={labelStyle}>{properties[v].title + units}</Text>
+            {row}
+          </View>
+        )
+        return
+      }
+      else {
+        if (!resource[v]  ||  !resource[v].length)
+          return;
+        var msgParts = utils.splitMessage(resource[v]);
+        // Case when the needed form was sent along with the message
+        if (msgParts.length === 2) {
+          var msgModel = utils.getModel(msgParts[1]);
+          if (msgModel) {
+            vCols.push(<View key={this.getNextKey()} style={styles.msgParts}>
+                         <Text style={style}>{msgParts[0]}</Text>
+                         <Text style={[style, {color: '#7AAAC3'}]}>{msgModel.value.title}</Text>
+                       </View>);
+            return;
+          }
+        }
+        row = <Text style={style} key={this.getNextKey()}>{resource[v]}</Text>;
+      }
+      vCols.push(
+        <View style={styles.refPropertyRow} key={this.getNextKey()}>
+          <Text style={labelStyle}>{properties[v].title + units}</Text>
+          {row}
+        </View>
+      );
+    });
+    // if (model.style)
+    //   vCols.push(<Text style={styles.verySmallLetters}>{model.title}</Text>);
+
+    if (vCols  &&  vCols.length) {
+      vCols.forEach((v) => {
+        renderedRows.push(v);
+      });
+    }
+  }
+
 }
 
 reactMixin(VerificationRow.prototype, RowMixin);
@@ -306,7 +458,7 @@ var styles = StyleSheet.create({
     // marginBottom: 2,
   },
   resourceTitleL: {
-    flex: 0.7,
+    // flex: 0.7,
     fontSize: 16,
     fontWeight: '400',
     paddingRight: 5,
