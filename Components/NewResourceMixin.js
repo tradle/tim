c84d668776123a6b3c35456486f80d8ac793d41f
pgmemk <ellen.katsnelson@lablz.com>
2017-10-02 16:03:32 -0400

    fix multiselect props: trying to set empty array as prop value bug

--- Components/NewResourceMixin.js
+++ Components/NewResourceMixin.js
@@ -1304,7 +1304,7 @@ var NewResourceMixin = {
     //   }
     // }
     let val = resource && resource[params.prop]
-    if (this.props.search  && Array.isArray(val)  &&  !val.length)
+    if (Array.isArray(val)  &&  !val.length)
       val = null
     if (val) {
       if (isPhoto) {
