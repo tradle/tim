e534bbf1f69f2e7b4eea82149c657b2e9b6aa8dc
pgmemk <ellen.katsnelson@lablz.com>
2017-10-06 20:28:05 -0400

    code cleanup

--- Components/FormRequestRow.js
+++ Components/FormRequestRow.js
@@ -257,7 +257,6 @@ class FormRequestRow extends Component {
     }
     if (oResource._context)
       resource._context = oResource._context
-    let self = this
     this.props.navigator.push({
       title: translate(prop),
       id: 33,
