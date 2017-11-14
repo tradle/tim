bc54740bdbc8fc19df394246dece3e215940d0e3
pgmemk <ellen@tradle.io>
2017-11-13 19:17:51 -0500

    forms for application fix

--- Store/Store.js
+++ Store/Store.js
@@ -3099,6 +3099,7 @@ var Store = Reflux.createStore({
       var key = utils.getId(rr)
 
       rr.to = self.buildRef(isReadOnlyContext ? context.to : r.to)
+      rr.from = rr.from || r.from
       if (isContext)
         rr.to.organization = self.buildRef(to)
 
@@ -10615,7 +10616,7 @@ var Store = Reflux.createStore({
       return
     let productToForms = {}
     allFormRequests.forEach((r) => {
-      if (r._documentCreated  &&  r._document) {
+      if (r._documentCreated  &&  r._document  &&  r.product) {
         var l = productToForms[r.product]
         if (!l) {
           l = {}
