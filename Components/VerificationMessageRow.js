eee90994c98f8b0fa2422a7a0de71db4668e49d9
pgmemk <ellen@tradle.io>
2017-12-01 22:01:45 -0500

    shared verification positioning fix

--- Components/VerificationMessageRow.js
+++ Components/VerificationMessageRow.js
@@ -232,7 +232,7 @@ class VerificationMessageRow extends Component {
     //   bg = bankStyle.BACKGROUND_COLOR
     var viewStyle = { margin: 1 } //, paddingRight: 10 }
     let contextId = this.getContextId(resource)
-    if (!isMyMessage) {
+    if (!isMyMessage  &&  !isShared) {
       messageBody = <View style={[chatStyles.row]}>
                       {this.getOwnerPhoto(isMyMessage)}
                       {messageBody}
