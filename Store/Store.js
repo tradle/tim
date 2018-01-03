4ab74e858d5170365052116f15075a373fc6a5fb
pgmemk <ellen@tradle.io>
2018-01-03 17:26:12 -0500

    params for getting all contexts missing the resource type

--- Store/Store.js
+++ Store/Store.js
@@ -6983,6 +6983,7 @@ var Store = Reflux.createStore({
       this.trigger({action: 'allContexts', list: contexts, to: params.to})
     }
     else {
+      extend(params, {modelName: PRODUCT_REQUEST})
       let list = await this.searchMessages(params)
       let l = list  &&  list.filter((r) => r._formsCount)
       this.trigger({action: 'allContexts', list: l, to: params.to})
