0644cf81e1da878b666162fe7b499cb8710dde6b
pgmemk <ellen@tradle.io>
2018-01-15 20:44:52 -0500

    positioning message fix

--- Components/FormMessageRow.js
+++ Components/FormMessageRow.js
@@ -196,10 +196,10 @@ class FormMessageRow extends Component {
                : <View/>
 
     let renderedRow = []
+    let isMyMessage = this.isMyMessage()
     let ret = this.formatRow(isMyMessage || isShared, renderedRow)
     let noContent = !hasSentTo &&  !renderedRow.length
 
-    let isMyMessage = this.isMyMessage()
     let isShared = this.isShared()
     // let isSharedContext = toChat  &&  utils.isContext(toChat[TYPE]) && resource._context  &&  utils.isReadOnlyChat(resource._context)
 
