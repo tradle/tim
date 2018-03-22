5682662ec3474a995df7852b7f290756c664e075
Mark Vayngrib <mark@tradle.io>
2018-03-22 14:31:19 -0400

    refactor: use react-native-carousel instead of react-native-gallery

--- Components/PhotoCarousel.js
+++ Components/PhotoCarousel.js
@@ -1,11 +1,18 @@
 console.log('requiring PhotoCarousel.js')
 'use strict'
 
-// import { makeResponsive } from 'react-native-orient'
-import Gallery from 'react-native-gallery'
-import utils from '../utils/utils'
+import Carousel from 'react-native-carousel';
+import constants from '@tradle/constants'
+import { makeResponsive } from 'react-native-orient'
+
+import {
+  StyleSheet,
+  Image,
+  View,
+} from 'react-native'
 
 import React, { Component } from 'react'
+import utils from '../utils/utils'
 
 class PhotoCarousel extends Component {
   static displayName = 'PhotoCarousel'
@@ -16,25 +23,111 @@ class PhotoCarousel extends Component {
     return utils.resized(this.props, nextProps)
   }
   render() {
-    var photoUrls = [];
-    var currentPhoto = this.props.currentPhoto || (this.props.photos && this.props.photos[0])
-    var currentPhotoIndex = -1;
-    let photos = (!this.props.photos  &&  currentPhoto) ? [currentPhoto] : this.props.photos
-    for (var i=0; i<photos.length; i++) {
-      var photo = photos[i];
+    let photoUrls = [];
+    let currentPhoto = this.props.currentPhoto || (this.props.photos && this.props.photos[0])
+    let currentPhotoIndex = -1;
+    let n = this.props.photos && this.props.photos.length || 1;
 
-      if (currentPhotoIndex === -1  &&  photo.url === currentPhoto.url)
-        currentPhotoIndex = i;
-      photoUrls.push(photo.url)
+    let model = utils.getModel(this.props.resource[constants.TYPE]);
+    let isVertical = currentPhoto.isVertical
+    let cnt = 2000
+    let r = this.props.resource
+    let styles = createStyles()
+    let photos = []
+    let {width, height} = utils.dimensions(PhotoCarousel)
+    for (let j=0; j<2; j++) {
+      for (let i=0; i<n; i++) {
+        let photo = this.props.photos[i];
+
+        if (currentPhotoIndex == -1  &&  photo.url !== currentPhoto.url)
+          continue;
+        if (currentPhotoIndex == -1)
+          currentPhotoIndex = i;
+        let key = r[constants.ROOT_HASH] + cnt++
+
+        if (photo.width) {
+          let w = width, h, padding
+
+          if (height > width) {
+            if (photo.width > width)
+              h = photo.height * width / photo.width
+            else {
+              h = photo.height
+              w = photo.width
+              padding = (width - photo.width) / 2
+            }
+
+            if (h > 0.8 * height)
+              h = Math.floor(0.8 * height)
+          }
+          else {
+            h = Math.floor(0.8 * height)
+            if (photo.height > h)
+              w = photo.width * h / photo.height
+            else {
+              h = photo.height
+              w = photo.width
+            }
+          }
+
+          photos.push(
+                        <View style={[styles.container]} key={key}>
+                          <Image resizeMode='contain' source={{uri: photo.url}} style={{width: w, height: h}}/>
+                        </View>
+                     )
+        }
+        else
+          photos.push(
+            isVertical  ? <View style={styles.container} key={key}>
+                            <Image resizeMode='contain' source={{uri: photo.url}} style={styles.imageV}/>
+                          </View>
+
+                        : <View style={styles.container} key={key}>
+                            <Image resizeMode='contain' source={{uri: photo.url}} style={styles.image}/>
+                          </View>
+        )
+      }
+      n = currentPhotoIndex
+    }
+    if (!photos.length) {
+      debugger
+      return <View/>
     }
     return (
-      <Gallery
-        style={{flex: 1, backgroundColor: 'black'}}
-        images={photoUrls}
-        initialPage={currentPhotoIndex}
-      />
+      <Carousel width={width}
+                loop={false}
+                animate={false}
+                indicatorOffset={20}
+                indicatorColor={'#efefef'}>
+        {photos}
+      </Carousel>
     );
   }
 }
-// PhotoCarousel = makeResponsive(PhotoCarousel)
+
+const createStyles = utils.styleFactory(PhotoCarousel, function ({ dimensions }) {
+  return StyleSheet.create({
+    image: {
+      width: dimensions.width,
+      height: dimensions.height - 200,
+    },
+    imageH: {
+      width: dimensions.width,
+      height: dimensions.width - 100
+    },
+    imageV: {
+      width: dimensions.width,
+      height: dimensions.height - 200,
+    },
+    container: {
+      width: dimensions.width,
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'black',
+    },
+  })
+})
+
+PhotoCarousel = makeResponsive(PhotoCarousel)
 module.exports = PhotoCarousel;
