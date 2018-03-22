b74735770ab773feb39d84633730fe5578d4fcb5
pgmemk <ellen@tradle.io>
2018-03-22 16:33:58 -0400

    text field in chat is not visible

--- utils/browser.js
+++ utils/browser.js
@@ -45,7 +45,8 @@ module.exports = {
 
     return ffPrivateDetector
   },
-  navBarHeight: isSafari || isIE ? 64 : 0
+  navBarHeight: 64
+  // navBarHeight: isSafari || isIE ? 64 : 0
 }
 
 // adapted from https://gist.github.com/cou929/7973956
