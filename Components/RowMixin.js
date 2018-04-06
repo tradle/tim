013935d2aee38f9c6a27b944121d47e2b827b6c1
pgmemk <ellen@tradle.io>
2018-04-06 17:30:32 -0400

    refactor: function isMyMessage split and common case moved to utils

--- Components/RowMixin.js
+++ Components/RowMixin.js
@@ -189,68 +189,69 @@ var RowMixin = {
       return utils.formatDate(resource.time);
   },
   isMyMessage(to) {
-    if (this.props.isAggregation)
+    let { resource, isAggregation, application } = this.props
+    if (isAggregation)
       return
-    let r = this.props.resource
-    if (this.props.application)
+    if (application)
       return false
-    if (r._inbound)
-      return false
-    if (r._outbound)
-      return true
-    let fromId = utils.getId(r.from);
-    let toId = utils.getId(r.to);
-    let me = utils.getMe()
-    let meId = utils.getId(me)
-    if (fromId === meId)
-      return true;
-    if (toId === meId)
-      return false
-
-    if (utils.getModel(r[TYPE]).subClassOf == MY_PRODUCT) {
-      let org = r.from.organization
-      if (org  &&  utils.getId(r.from.organization) !== utils.getId(this.props.to))
+    if (utils.getModel(resource[TYPE]).subClassOf == MY_PRODUCT) {
+      let org = resource.from.organization
+      if (org  &&  utils.getId(resource.from.organization) !== utils.getId(this.props.to))
         return true
     }
-    if (me.isEmployee) {
-      if (r.from.organization  &&  me.organization.id !== r.from.organization.id)
-        return false
-      if (r._context) {
-        // check if the employee is the applicant
-        let cFrom = r._context.from
-        if (utils.getId(cFrom) === meId)
-          return true
-        if (cFrom.organization) {
-          if (cFrom.organization.id === me.organization.id)
-            return utils.getId(r._context.to) !== meId
-        }
-      }
-      let myOrgId = utils.getId(me.organization)
-      // bot -> bot
-      if (r.from.organization  &&
-          r.to.organization    &&
-          r.from.organization.id === r.to.organization.id  &&
-          r.from.organization.id === myOrgId) {
-        return false
-      }
+    return utils.isMyMessage({resource, to})
+    // if (r._inbound)
+    //   return false
+    // if (r._outbound)
+    //   return true
+    // let fromId = utils.getId(r.from);
+    // let toId = utils.getId(r.to);
+    // let me = utils.getMe()
+    // let meId = utils.getId(me)
+    // if (fromId === meId)
+    //   return true;
+    // if (toId === meId)
+    //   return false
 
-      if (r.from.organization) {
-        if (myOrgId === utils.getId(r.from.organization)) {
-          if (to  &&  utils.getId(to) === myOrgId)
-            return false
-          else
-            return true
-        }
-      }
-      if (r._context) {
-        if (r._context.from) {
-          let fOrg = r._context.from.organization
-          let applier = fOrg  &&  utils.getId(fOrg) === utils.getId(me.organization)
-          if (applier  &&  fOrg === utils.getId(r.from.organization))
-            return true
-        }
-      }
-    }
+    // if (me.isEmployee) {
+    //   if (r.from.organization  &&  me.organization.id !== r.from.organization.id)
+    //     return false
+    //   if (r._context) {
+    //     // check if the employee is the applicant
+    //     let cFrom = r._context.from
+    //     if (utils.getId(cFrom) === meId)
+    //       return true
+    //     if (cFrom.organization) {
+    //       if (cFrom.organization.id === me.organization.id)
+    //         return utils.getId(r._context.to) !== meId
+    //     }
+    //   }
+    //   let myOrgId = utils.getId(me.organization)
+    //   // bot -> bot
+    //   if (r.from.organization  &&
+    //       r.to.organization    &&
+    //       r.from.organization.id === r.to.organization.id  &&
+    //       r.from.organization.id === myOrgId) {
+    //     return false
+    //   }
+
+    //   if (r.from.organization) {
+    //     if (myOrgId === utils.getId(r.from.organization)) {
+    //       if (to  &&  utils.getId(to) === myOrgId)
+    //         return false
+    //       else
+    //         return true
+    //     }
+    //   }
+    //   if (r._context) {
+    //     if (r._context.from) {
+    //       let fOrg = r._context.from.organization
+    //       let applier = fOrg  &&  utils.getId(fOrg) === utils.getId(me.organization)
+    //       if (applier  &&  fOrg === utils.getId(r.from.organization))
+    //         return true
+    //     }
+    //   }
+    // }
   },
   isShared() {
     let { resource, to, application } = this.props
