3ec1ceca6286863f1dfaa8a6730b7e2999d48651
pgmemk <ellen@tradle.io>
2018-04-21 01:24:07 -0400

    fix: minor change

--- Store/Store.js
+++ Store/Store.js
@@ -8706,7 +8706,7 @@ var Store = Reflux.createStore({
       }
     }
     if (iKey === meId) {
-      if (!value.photos.length)
+      if (!value.photos  ||  !value.photos.length)
         value.photos = utils.clone(sampleProfile.photos)
     }
 
