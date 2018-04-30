eb6a40269ba2b26f37e874c6e2fb55f35b41b1b9
pgmemk <ellen@tradle.io>
2018-04-30 11:37:12 -0400

    fix: provider styles were not propagated; wrong color for nav bar title

--- Components/MessageView.js
+++ Components/MessageView.js
@@ -192,7 +192,6 @@ class MessageView extends Component {
               title: translate(model),
               id: 4,
               component: NewResource,
-              titleTextColor: '#7AAAC3',
               backButtonTitle: 'Back',
               rightButtonTitle: 'Done',
               passProps: {
@@ -243,12 +242,11 @@ class MessageView extends Component {
       title: model.title,
       id: 4,
       component: NewResource,
-      titleTextColor: '#7AAAC3',
       backButtonTitle: 'Back',
       rightButtonTitle: 'Done',
       passProps: {
         model: model,
-        bankStyle: this.state.bankStyle || bankStyle,
+        bankStyle: /*this.state.bankStyle ||*/ bankStyle,
         resource: r,
         prop: itemBl,
         search,
