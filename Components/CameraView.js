31bfa9526a68cb3bc30b8bb3e14996fdad38b36b
pgmemk <ellen@tradle.io>
2018-08-28 19:35:55 -0400

    fix: alignments; feat: localization project

--- Components/CameraView.js
+++ Components/CameraView.js
@@ -13,8 +13,9 @@ import PropTypes from 'prop-types'
 // import Camera from 'react-native-camera'
 import { RNCamera } from 'react-native-camera'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { makeResponsive } from 'react-native-orient'
 
-import utils from '../utils/utils'
+import utils, { translate } from '../utils/utils'
 const BASE64_PREFIX = 'data:image/jpeg;base64,'
 
 class CameraView extends Component {
@@ -30,15 +31,16 @@ class CameraView extends Component {
   render() {
     let data = this.state.data
     if (data) {
-      let { width, height } = utils.dimensions()
-      return <View style={[styles.container, {backgroundColor: '#000'}]}>
-                <Image source={{isStatic: true, url: utils.BASE_64_PREFIX + data.base64}} style={{width, height: height - 60}} />
-                <View style={{flexDirection: 'row', width, justifyContent: 'center'}}>
+      // debugger
+      let { width, height } = utils.dimensions(CameraView)
+      return <View style={[styles.container, {backgroundColor: '#000', justifyContent: 'center'}]}>
+                <Image source={{uri: 'data:image/jpeg;base64,' + data.base64}} style={{width: width, height: height - 80}} />
+                <View style={styles.footer1}>
                    <TouchableOpacity onPress={() => this.setState({data: null})}>
-                     <Text style={styles.cancel}>Re-take</Text>
+                     <Text style={styles.cancel}>{translate('retake')}</Text>
                    </TouchableOpacity>
                    <TouchableOpacity onPress={() => this.onTakePic({...data})}>
-                     <Text style={styles.useIt}>Use photo</Text>
+                     <Text style={styles.useIt}>{translate('usePhoto')}</Text>
                     </TouchableOpacity>
                 </View>
               </View>
@@ -57,7 +59,7 @@ class CameraView extends Component {
             type={this.state.cameraType}>
           </RNCamera>
           <View style={styles.footer}>
-            <Text style={styles.currentAction}>PHOTO</Text>
+            <Text style={styles.currentAction}>{translate('PHOTO')}</Text>
             <TouchableOpacity onPress={this._takePicture.bind(this)}>
                <Icon name='ios-radio-button-on'  size={85}  color='#eeeeee'  style={styles.icon}/>
             </TouchableOpacity>
@@ -65,7 +67,7 @@ class CameraView extends Component {
               <Icon name='ios-reverse-camera-outline' size={50} color='#eeeeee' />
             </TouchableOpacity>
             <TouchableOpacity onPress={() => this.props.navigator.pop()} style={styles.left}>
-              <Text style={{fontSize: 20, color: '#ffffff'}}>Cancel</Text>
+              <Text style={{fontSize: 20, color: '#ffffff'}}>{translate('cancel')}</Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -111,7 +113,7 @@ class CameraView extends Component {
     })
   }
 }
-
+CameraView = makeResponsive(CameraView)
 
 var styles = StyleSheet.create({
   container: {
@@ -120,6 +122,14 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     alignSelf: 'stretch',
     backgroundColor: 'transparent',
+  },
+  footer1: {
+    backgroundColor: '#000000',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    paddingTop: 30,
+    flex: 1,
   },
   footer: {
     backgroundColor: '#000000',
@@ -138,19 +148,23 @@ var styles = StyleSheet.create({
     alignSelf: 'center',
   },
   cancel: {
-    position: 'absolute',
-    right: 60,
-    top: 20,
+    // position: 'absolute',
+    // right: 60,
+    // top: 20,
     // backgroundColor: 'transparent',
+    paddingHorizontal: 20,
     color: '#FFF',
     fontWeight: '600',
+    alignSelf: 'center',
     fontSize: 20,
   },
   useIt: {
-    position: 'absolute',
-    left: 60,
-    top: 20,
+    // position: 'absolute',
+    // left: 60,
+    // top: 20,
     // backgroundColor: 'transparent',
+    paddingHorizontal: 20,
+    alignSelf: 'center',
     color: '#FFF',
     fontWeight: '600',
     fontSize: 20,
