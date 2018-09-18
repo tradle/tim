d0c892fa6c0d65001ecf684c9c43c9cb57368396
pgmemk <ellen@tradle.io>
2018-09-18 13:11:06 -0400

    fix: code optimizations

--- Components/VerificationMessageRow.js
+++ Components/VerificationMessageRow.js
@@ -62,11 +62,6 @@ class VerificationMessageRow extends Component {
     let model = utils.getModel(resource[TYPE]);
     let renderedRow = [];
 
-    var time = this.getTime(resource);
-    var date = time
-             ? <Text style={chatStyles.date} numberOfLines={1}>{time}</Text>
-             : <View />;
-
     var isMyMessage = this.isMyMessage();
 
     var dType = utils.getType(resource.document)
@@ -250,7 +245,7 @@ class VerificationMessageRow extends Component {
 
     return (
       <View key={this.getNextKey()}>
-        {date}
+        {this.getChatDate(resource)}
         {messageBody}
         {contextId}
       </View>
