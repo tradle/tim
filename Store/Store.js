09074b9a6a63a1d04f479922e163a51b787c1834
Mark Vayngrib <mark@tradle.io>
2018-10-02 22:06:50 -0400

    fix: add a safety check to custom indexing

--- Store/Store.js
+++ Store/Store.js
@@ -1048,8 +1048,14 @@ var Store = Reflux.createStore({
         'typeAndTime'
       ],
       getProps: function (wrapper) {
-        const payload = wrapper.object.object
         const props = {}
+        const msg = wrapper.object
+        const payload = msg && msg.object
+        if (!payload) {
+          debug(`[ERROR] unable to index, missing ${msg ? 'payload' : 'message'} body`, JSON.stringify(wrapper))
+          return props
+        }
+
         // get payload
         const model = self.getModel(payload[TYPE])
         if (!model)
