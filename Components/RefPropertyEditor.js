8ca3c15fff8da755832c63355a7503e96a9cb633
pgmemk <ellen@tradle.io>
2019-02-06 17:01:27 -0500

    fix: change alert

--- Components/RefPropertyEditor.js
+++ Components/RefPropertyEditor.js
@@ -295,7 +295,7 @@ class RefPropertyEditor extends Component {
             // this.showBlinkIDScanner(pName)
           }
           else
-            Alert.alert(translate('pleaseChooseCountryAndDT'))
+            Alert.alert(translate('pleaseChooseDT'))
           return
         }
       }
