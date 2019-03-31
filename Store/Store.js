accc89ca0a4a513974cb9ba9bbe70495a4286bb7
pgmemk <ellen@tradle.io>
2019-03-31 12:06:55 -0400

    fix: language setting; locale on the phone might have a different language code than the google supported language codes ex. filipino language from device it is 'fil', from google - 'pl'

--- Store/Store.js
+++ Store/Store.js
@@ -2286,9 +2286,13 @@ debug('sent:', r)
         let locale = ENV.locale
         if (locale) {
           languageCode = locale.language
-          if (languageCode.length > 2  &&  languageCode !== 'zh-TW')
+          if (languageCode.length === 3) {
+            if (languageCode === 'fil')
+              languageCode = 'tl'
+          }
+          else if (languageCode.length > 2  &&  languageCode !== 'zh-TW')
             languageCode = languageCode.split('-')[0]
-          language = this._getItem(`${LANGUAGE}_${language}`)
+          language = utils.buildStubByEnumTitleOrId(utils.getModel(LANGUAGE), languageCode)
         }
       }
     }
