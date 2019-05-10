41f1df77b0e6f1cba55e86a88e7bfd3f33b74fb4
pgmemk <ellen@tradle.io>
2019-05-10 14:05:16 -0400

    fix: allow showing multientry enum props

--- Components/ShowRefList.js
+++ Components/ShowRefList.js
@@ -33,7 +33,8 @@ const {
   VERIFICATION,
   ORGANIZATION,
   PROFILE,
-  FORM
+  FORM,
+  ENUM
 } = constants.TYPES
 
 class ShowRefList extends Component {
@@ -345,8 +346,13 @@ function hasPropertiesToShow(resource) {
   })
 
   for (let p in resource) {
-    if (!props[p]  ||  p.charAt(0) === '_'  ||  props[p].type === 'array')
+    if (!props[p]  ||  p.charAt(0) === '_')//  ||  props[p].type === 'array')
       continue
+    if (props[p].type === 'array') {
+      const pref = props[p].items.ref
+      if (!pref  ||  utils.getModel(pref).subClassOf !== ENUM)
+        continue
+    }
     if (vCols  &&  vCols.indexOf(p) !== -1)
       return true
   }
