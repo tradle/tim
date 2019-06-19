92130b7ab7872ea8e49b7120e95b866baa5bb741
pgmemk <ellen@tradle.io>
2019-06-18 23:54:55 -0400

    feat: allow metadata for requested props

--- Store/Store.js
+++ Store/Store.js
@@ -3389,7 +3389,10 @@ debug('sent:', r)
       let requestedProperties = moreInfo.requestedProperties
       if (requestedProperties) {
         requestedProperties.forEach((r) => {
-          rprops[r.name] = r.message || ''
+          rprops[r.name] = {
+            message: r.message || '',
+            required: r.required
+          }
         })
       }
     }
