d188be60030d2aa8a4b58717a9970092745bb915
pgmemk <ellen@tradle.io>
2019-06-29 15:51:27 -0400

    feat: proof of address

---
+++ plugins/ProofOfAddress.js
@@ -0,0 +1,55 @@
+import { TYPE } from '@tradle/constants'
+import { getModel, getPropertiesWithAnnotation, isSubclassOf } from '../utils/utils'
+
+const COUNTRY = 'tradle.Country'
+const FORM = 'tradle.Form'
+const PHONE_BILL = 'tradle.PhoneBill'
+const PROOF_OF_ADDRESS = 'tradle.ProofOfAddress'
+
+module.exports = function ProofOfAddress ({ models }) {
+  return {
+    validateForm: function validateForm ({
+      application,
+      form
+    }) {
+      if (!application) return
+
+      let model = getModel(form[TYPE])
+      if (!isSubclassOf(model, PROOF_OF_ADDRESS))
+        return
+      return getPropsForLegalDocumentI(form)
+    }
+  }
+}
+function getPropsForLegalDocumentI(form) {
+  if (!form.document  ||  !form.country  ||  !form.accountName)
+    return {
+      requestedProperties: [
+        { name: 'country' },
+        { name: 'document' }
+      ]
+    }
+  let propsArr = [
+    'accountName',
+    'billDate',
+    'issuer',
+    'issuerPhoneNumber',
+    'streetAddress',
+    'city',
+    'town',
+    'postalCode',
+    'country',
+    'document',
+    'referenceNo',
+    'phoneNumber',
+    'accountNumber'
+  ]
+  let props = getModel(form[TYPE]).properties
+  let requestedProperties = []
+  propsArr.forEach(p => {
+    if (props[p])
+      requestedProperties.push({ name: p })
+  })
+  return { requestedProperties }
+}
+
