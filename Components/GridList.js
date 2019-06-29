9c289e8dcdc659330ce36ce4323020aac7ce76b2
pgmemk <ellen@tradle.io>
2019-06-29 15:42:04 -0400

    fix: no grid for models

--- Components/GridList.js
+++ Components/GridList.js
@@ -1545,9 +1545,9 @@ console.log('GridList._loadMoreContentAsync: filterResource', resource)
     Actions.addChatItem({resource: resource})
   }
   renderHeader() {
-    let { search, modelName } = this.props
-    if (!search)
-      return
+    let { search, modelName, isModel } = this.props
+    if (!search || isModel)
+       return
     if (modelName !== PROFILE) {
       if (this.state.isGrid  &&  !utils.isContext(modelName))
         return this.renderGridHeader()
