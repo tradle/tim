0bbd0bb509cb721f301f850e125cb231f3dcc35c
pgmemk <ellen@tradle.io>
2019-07-13 15:39:18 -0400

    refactor: border around the photo is too vivid

--- Components/MessageView.js
+++ Components/MessageView.js
@@ -575,7 +575,7 @@ var createStyles = utils.styleFactory(MessageView, function ({ dimensions, bankS
     photoBG: {
       backgroundColor: '#f7f7f7',
       alignItems: 'center',
-      borderColor: bankStyle.linkColor,
+      borderColor: '#cccccc',
       borderTopWidth: 1,
       borderBottomWidth: 1
     },
