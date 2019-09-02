bf9bdcf7c050d6e986e63600ac0b9c490009105e
pgmemk <ellen@tradle.io>
2019-09-02 11:29:28 -0400

    fix: empty page when click on logo

--- index.common.js
+++ index.common.js
@@ -584,7 +584,13 @@ var NavigationBarRouteMapper = {
         photo = <Image source={{uri: uri}} style={[styles.msgImageNoText, platformStyles.logo, {width, height}]} />
 
 
-      let provider = resource  &&  resource[TYPE] === ORGANIZATION  ||  to  ||  (resource && resource.to && resource.to.organization)
+      let provider
+      if (resource  &&  resource[TYPE] === ORGANIZATION)
+        provider = resource
+      else if (to)
+        provider =  to
+      else if (bankStyle  &&  resource && resource.to && resource.to.organization)
+        provider = resource.to.organization
       if (provider)
         photo = <TouchableOpacity hitSlop={HIT_SLOP} onPress={() => this.showProvider(route, provider, navigator)}>{photo}</TouchableOpacity>
     }
