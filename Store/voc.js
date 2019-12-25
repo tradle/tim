2e51a67c8110fe15f23bee9e573de29bf950680b
pgmemk <ellen@tradle.io>
2019-12-25 11:35:34 -0500

    feat: dynamically add all tradle.Form backlink props to its subclasses

--- Store/voc.js
+++ Store/voc.js
@@ -1,4 +1,6 @@
 import _ from 'lodash'
+import { TYPES } from '@tradle/constants'
+const { FORM } = TYPES
 
 let models = _.cloneDeep(_.extend(
   {},
@@ -11,24 +13,31 @@ let models = _.cloneDeep(_.extend(
 ))
 
 const exclude = ['tradle.AssignRelationshipManager', 'tradle.ProductRequest']
+
+let formBacklinks = []
+let formProps = models[FORM].properties
+for (let p in formProps) {
+  let prop = formProps[p]
+  if (prop.items && prop.items.backlink) formBacklinks.push({[p]: prop})
+}
 for (let m in models) {
   if (exclude.includes(m)) continue
   let model = models[m]
-  if (model.subClassOf !== 'tradle.Form' ||
-      model.properties.modificationHistory) continue
-  _.extend(model.properties, {
-    modificationHistory: {
-      type: 'array',
-      internalUse: true,
-      readOnly: true,
-      title: 'Data Lineage',
-      icon: 'md-list',
-      items: {
-        ref: 'tradle.Modification',
-        backlink: 'form'
-      }
-    }
-  })
+  if (model.abstract  ||  !model.subClassOf)
+    continue
+  let sub = model
+  while (sub.subClassOf  &&  sub.subClassOf !== FORM)
+    sub = models[sub.subClassOf]
+
+  if (sub.subClassOf) {
+    formBacklinks.forEach(bl => {
+      let p = Object.keys(bl)[0]
+      if (!model.properties[p])
+       _.extend(model.properties, {
+          [p]: bl[p]
+        })
+    })
+  }
 }
 
 module.exports = models
