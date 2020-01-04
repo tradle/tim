1eb30c5ab958a947e95963bb9b782d54e02e012a
pgmemk <ellen@tradle.io>
2020-01-04 13:50:37 -0500

    fix: request for corrections: can't request any props to correct

--- Components/ShowPropertiesView.js
+++ Components/ShowPropertiesView.js
@@ -152,7 +152,7 @@ class ShowPropertiesView extends Component {
     let isPartial = model.id === PARTIAL
     let isMethod = utils.isSubclassOf(model, METHOD)
     let me = utils.getMe()
-    if (me.isEmployee  &&  resource._sourceOfData) {
+    if (me.isEmployee  &&  !checkProperties  && resource._sourceOfData) {
       vCols.push('_sourceOfData')
       // vCols.push('_dataLineage')
     }
@@ -230,6 +230,10 @@ class ShowPropertiesView extends Component {
               </View>
             )
             return
+          }
+          else if (pMeta.items) {
+            if (pMeta.items.ref  &&  !utils.isEnum(pMeta.items.ref))
+              return
           }
           val = <Text style={styles.title}>{NOT_SPECIFIED}</Text>
         }
