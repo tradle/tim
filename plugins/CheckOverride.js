5f3b4a7df24b8d983589feb66c3a39bacc3be001
pgmemk <ellen@tradle.io>
2020-01-30 19:47:19 -0500

    feat: more rules what props to show

--- plugins/CheckOverride.js
+++ plugins/CheckOverride.js
@@ -11,24 +11,55 @@ module.exports = function CheckOverride ({ models }) {
     }) {
       if (!application)
         return
-      if (getModel(form[TYPE]).subClassOf !== CHECK_OVERRIDE)
+      let m = getModel(form[TYPE])
+      if (m.subClassOf !== CHECK_OVERRIDE)
         return
 
       const { status, check } = form
       if (!status) {
+        if (m.editCols) {
+          return {
+            requestedProperties: m.editCols.map(p => {name: p})
+          }
+        }
         return {
           requestedProperties: [
-            { name: 'status' }
+            { name: 'status' },
+            { name: 'explanationForOverride' }
           ]
         }
       }
-      const reasons = status.title === 'Pass'  &&  'reasonsToPass' || 'reasonsToFail'
-      return {
-        requestedProperties: [
-          { name: 'status' },
-          { name: reasons }
-        ]
+      let requestedProperties
+      if (m.editCols) {
+        if (m.editCols) {
+          requestedProperties = {
+            requestedProperties: m.editCols.map(p => {name: p})
+          }
+        }
       }
+      else
+        requestedProperties = [
+            { name: 'status' },
+            { name: 'explanationForOverride' }
+          ]
+
+      let props = m.properties
+      if (!props.reasonToFail  ||  !props.reasonToPass)
+        return { requestedProperties }
+
+      const reasonSkip = status.title === 'Pass'  &&  'reasonsToFail' || 'reasonsToPass'
+      const reasonShow = status.title === 'Pass'  &&  'reasonsToPass' || 'reasonsToFail'
+      requestedProperties.push({ name: reasonShow })
+      for (let p in props) {
+        if (p === reasonSkip  ||  p === 'status'  ||  p === reasonShow)
+          continue
+        if (p.charAt(0) === '_')
+          continue
+        if (props[p].readOnly  ||  props[p].hidden)
+          continue
+        requestedProperties.push({name: p})
+      }
+      return { requestedProperties }
     }
   }
 }
