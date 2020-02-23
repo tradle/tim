8ae9f6296127c5c87bf8b7e9f89f4fc9f68da5f8
pgmemk <ellen@tradle.io>
2020-02-23 18:09:42 -0500

    refactor: hide typeOfControllingPerson from data entry

--- plugins/BusinessAccount.js
+++ plugins/BusinessAccount.js
@@ -197,7 +197,7 @@ function getPropsForControllingEntity(form) {
     let retProps = {
       requestedProperties: [
         {name: 'name', required: true},
-        {name: 'typeOfControllingPerson'},
+        // {name: 'typeOfControllingPerson'},
         {name: 'natureOfControl'},
         {name: 'percentageOfOwnership'},
         {name: 'sourceOfWealth', required: isFund  &&  isLimitedPartner},
