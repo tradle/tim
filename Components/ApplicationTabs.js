797b7484eb46a31a74c6bc33cdf0a311508e1d0b
pgmemk <ellen@tradle.io>
2020-03-11 19:06:13 -0400

    feat: allow/deny approval of the application project

--- Components/ApplicationTabs.js
+++ Components/ApplicationTabs.js
@@ -210,7 +210,11 @@ class ApplicationTabs extends Component {
                                     bankStyle={bankStyle}
                                     excludedProperties={['photos']}
                                     navigator={navigator} />
-      if (/*!resource.draft  &&*/ utils.isRM(resource)  &&  (resource.status !== 'approved' && resource.status !== 'denied')) {
+      if (utils.isRM(resource)           &&
+          resource.submissions           &&
+          /*!resource.draft  &&*/
+          resource.status !== 'approved' &&
+          resource.status !== 'denied') {
         details = <View style={styles.buttonsFooter}>
                    {details}
                    <View style={styles.buttons}>
