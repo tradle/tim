f8c6d92af11a00421d9d8193fa863843d3c71028
pgmemk <ellen@tradle.io>
2020-04-04 20:35:48 -0400

    fix: typo

--- plugins/CheckOverride.js
+++ plugins/CheckOverride.js
@@ -16,31 +16,36 @@ module.exports = function CheckOverride ({ models }) {
       if (subClassOf !== CHECK_OVERRIDE)
         return
 
-      const { status, check } = form
-
       let requestedProperties
-      if (m.editCols)
-        requestedProperties = m.editCols.map(p => ({name: p}))
-      else {
-        let notes = properties.explanationForOverride || properties.reviewNotes || properties.notes
-        requestedProperties = [
-          { name: 'status' },
-          { name: notes.name }
-        ]
+      if (m.editCols) {
+        requestedProperties = m.editCols.map(p => {
+            return { name: p }
+          })
       }
+      else {
+        requestedProperties = [
+            { name: 'status' },
+            { name: notes.name }
+          ]
+      }
+      const { status, check } = form
+      if (!status)
+        return { requestedProperties }
 
-      if (!status || !properties.reasonsToFail  ||  !properties.reasonsToPass)
+      let notes = properties.explanationForOverride || properties.reviewNotes || properties.notes
+      let props = m.properties
+      if (!props.reasonToFail  ||  !props.reasonToPass)
         return { requestedProperties }
 
       const reasonSkip = status.title === 'Pass'  &&  'reasonsToFail' || 'reasonsToPass'
       const reasonShow = status.title === 'Pass'  &&  'reasonsToPass' || 'reasonsToFail'
       requestedProperties.push({ name: reasonShow })
-      for (let p in properties) {
+      for (let p in props) {
         if (p === reasonSkip  ||  p === 'status'  ||  p === reasonShow)
           continue
         if (p.charAt(0) === '_')
           continue
-        if (properties[p].readOnly  ||  properties[p].hidden)
+        if (props[p].readOnly  ||  props[p].hidden)
           continue
         requestedProperties.push({name: p})
       }
