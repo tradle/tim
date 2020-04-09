15512866e8b29e71a9ae426e6c2385f5d3e02a23
pgmemk <ellen@tradle.io>
2020-04-08 20:58:21 -0400

    feat: allow product icons

--- Components/StringRow.js
+++ Components/StringRow.js
@@ -23,7 +23,7 @@ class StringRow extends Component {
     let { title, bankStyle, description, forScan, icon } = this.props
     let styles = createStyles({bankStyle, description})
     if (icon)
-      icon = <Icon name={icon} size={45} color={DEFAULT_PRODUCT_ROW_TEXT_COLOR} />
+      icon = <Icon name={icon} size={45} color={DEFAULT_PRODUCT_ROW_TEXT_COLOR} style={{paddingTop: 2, paddingBottom: 5, paddingLeft: 15}}/>
     let d
     if (description)
       d = <Text style={styles.modelDescription}>{description}</Text>
@@ -46,8 +46,8 @@ var createStyles = utils.styleFactory(StringRow, function ({ bankStyle, descript
     row: {
       flexDirection: 'row',
       justifyContent: 'center',
-      marginTop: 15,
-      marginBottom: description ? 7 : 15,
+      // marginTop: 15,
+      // marginBottom: description ? 7 : 15,
       marginLeft: 15,
     },
     modelTitle: {
