ec6b41f88aed52f4218d0c243b4084107f36b8b9
pgmemk <ellen@tradle.io>
2020-04-17 13:12:08 -0400

    feat: pairing devices

--- Store/Store.js
+++ Store/Store.js
@@ -5437,13 +5437,14 @@ if (!res[SIG]  &&  res._message)
       throw new Error('not found')
 
     debugger
+    me._masterAuthor = masterAuthor
+    await this.setMe(me)
+    Actions.showModal({title: translate('pairing'), showIndicator: true})
     let { list } = await this.searchServer({
         modelName: MY_EMPLOYEE_PASS,
         noTrigger: true,
         filterResource: {owner: {id: `${IDENTITY}_${masterAuthor}_${masterAuthor}`}}
       })
-
-    me._masterAuthor = masterAuthor
 
     this.trigger({action: 'masterIdentity', masterAuthor, me})
     if (!list  ||  !list.length)
@@ -5453,6 +5454,7 @@ if (!res[SIG]  &&  res._message)
       let org = this._getItem(myEmployeeBadge.from).organization
       await this.setupEmployee(myEmployeeBadge, org)
     }
+    Actions.hideModal()
   },
   async insurePublishingIdentity(org) {
     if (!me)
