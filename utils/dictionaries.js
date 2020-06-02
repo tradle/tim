f4bc661b709b8f928ab6ac3eaed4f505d802eb9b
pgmemk <ellen@tradle.io>
2020-06-01 23:19:35 -0400

    refactor: code cleanup

--- utils/dictionaries.js
+++ utils/dictionaries.js
@@ -1,9 +1,6 @@
 import _ from 'lodash'
 import { fetchWithBackoff } from './utils'
 
-// const dictionariesM = require('@tradle/models').dictionaries
-// const dictionariesCM = require('@tradle/custom-models').dictionaries
-// const dictionariesMCO = require('@tradle/models-corporate-onboarding').dictionaries
 const MODEL = 'model'
 const PROPERTY_NAME = 'propertyName'
 const URL = 'https://s3.eu-west-2.amazonaws.com/tradle.io/dictionaries/'
@@ -53,19 +50,6 @@ async function dictionaries({lang, dictionaryDomains, providerDictionaries}) {
 
   return d
 }
-// function dictionaries1(lang) {
-//   let d = {models: {}, properties: {}, enums: {}}
-//   let dM = dictionariesM  &&  dictionariesM[lang]  &&  genDictionary(dictionariesM[lang], lang, d.enums)
-//   if (dM)
-//     _.extend(d, dM)
-// // debugger
-//   let dCM = dictionariesCM  &&  dictionariesCM[lang]  &&  genDictionary(dictionariesCM[lang], lang, d.enums)
-//   _.merge(d, dCM)
-//   let dMCO = dictionariesMCO  &&  dictionariesMCO[lang]  &&  genDictionary(dictionariesMCO[lang], lang, d.enums)
-//   _.merge(d, dMCO)
-
-//   return d
-// }
 function genDictionary(dictionary, lang, enums) {
   let groups = _.groupBy(dictionary, 'type')
   let models = groups[MODEL]
