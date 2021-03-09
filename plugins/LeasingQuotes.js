28cb0303ddb4ab157f68f73b15fbfd77b7ff3df6
pgmemk <ellen@tradle.io>
2021-03-09 11:54:11 -0500

    feat: leasingQuotes plugin added

---
+++ plugins/LeasingQuotes.js
@@ -0,0 +1,45 @@
+import { TYPE } from '@tradle/constants'
+import { getModel } from '../utils/utils'
+import extend from 'lodash/extend'
+
+const QUOTATION_DETAILS = '.QuotationDetails'
+
+module.exports = function CheckOverride ({ models }) {
+  return {
+    validateForm: function validateForm ({
+      application,
+      form
+    }) {
+      const ftype = form[TYPE]
+      if (!application || ftype.indexOf(QUOTATION_DETAILS) === -1)
+        return
+      let m
+      try {
+        m = getModel(ftype)
+      } catch (e) {
+        return
+      }
+      let { properties } = m
+
+      let requestedProperties
+      if (!form.term) {
+        requestedProperties = [{name: 'term'}]
+        return { requestedProperties }
+      }
+
+      let terms = form.terms
+
+      let termId = form.term.id
+      let term = terms.find(t => t.term.id === termId)
+
+      extend(form, term)
+
+      if (m.editCols) {
+        requestedProperties = m.editCols.map(p => {
+            return { name: p }
+          })
+      }
+      return { requestedProperties } || []
+    }
+  }
+}
