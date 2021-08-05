88c885bd54fcb562442679200338bd0c45f48f80
pgmemk <ellen@tradle.io>
2021-08-05 14:54:11 -0400

    fix: improvements

--- Components/ApplicantLegalEntityConsent.js
+++ Components/ApplicantLegalEntityConsent.js
@@ -43,6 +43,7 @@ class ApplicantLegalEntityConsent extends Component {
     let { resource } = this.props
     let bankStyle = defaultBankStyle
     let model = getModel(resource[TYPE])
+    let properties = model.properties
 
     let styles = createStyles({bankStyle})
 
@@ -59,6 +60,26 @@ class ApplicantLegalEntityConsent extends Component {
                   <Text style={styles.dateLabel}>{translate('dateSubmitted')}</Text>
                   <Text style={[styles.dateValue]}>{dateformat(new Date(resource._time), 'mmm d, yyyy')}</Text>
                 </View>
+    let p = ['declaration', 'certification']
+
+    let props = p.map(p => {
+      let val = translate(resource[p] && 'Yes' || 'No')
+      if (properties[p].description)
+        return <View style={styles.description}>
+                 <Markdown markdownStyles={getMarkdownStyles(bankStyle, false, false, true)} passThroughProps={{navigator, bankStyle}}>
+                   {translate(properties[p], model, true)}
+                 </Markdown>
+                 <Text style={styles.title}>{val}</Text>
+               </View>
+      else {
+        return <View style={styles.description}>
+                 <Text style={styles.title}>{translate(properties[p], model)}</Text>
+                 <Text style={styles.title}>{val}</Text>
+               </View>
+      }
+    })
+
+
     // return (
     //   <PDFViewer>
     //     <Document>
@@ -79,10 +100,14 @@ class ApplicantLegalEntityConsent extends Component {
     let width = utils.getContentWidth()
     return (
       <ScrollView style={{width, alignSelf: 'center'}}>
-        <View>
+        <View style={styles.pageTitle}>
+          <Text style={styles.bigTitle}>{translate(model)}</Text>
           {date}
+        </View>
+        <View>
           {description}
           {signature}
+          {props}
         </View>
       </ScrollView>
     )
@@ -96,10 +121,24 @@ var createStyles = utils.styleFactory(ApplicantLegalEntityConsent, function ({ d
       fontSize: 16,
       // fontFamily: 'Avenir Next',
       marginHorizontal: 7,
-      color: '#9b9b9b'
+      color: '#555555',
+      fontWeight: '600'
+    },
+    pageTitle: {
+      // alignSelf: 'center',
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+    },
+    bigTitle: {
+      paddingTop: 25,
+      fontSize: 22,
+      // fontFamily: 'Avenir Next',
+      marginHorizontal: 7,
+      color: '#555555'
     },
     date: {
-      padding: 10,
+      // padding: 10,
+      padding: 25,
       flexDirection: 'row',
       alignSelf: 'flex-end'
     },
