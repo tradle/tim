a65bee20ecb4c33ed425e397730a63c5f119871e
pgmemk <ellen@tradle.io>
2021-08-24 13:51:59 -0400

    fix: alignment for sig and markdown props

--- Components/NewResourceMixin.js
+++ Components/NewResourceMixin.js
@@ -806,7 +806,8 @@ var NewResourceMixin = {
     let title, sig
     if (value) {
       let vStyle = { height: 100, justifyContent: 'space-between', margin: 10, borderBottomColor: '#cccccc', borderBottomWidth: 1}
-      let lStyle = [styles.labelStyle, { paddingBottom: 10, color: lcolor, fontSize: 12}]
+      // let lStyle = [styles.labelStyle, { paddingBottom: 10, color: lcolor, fontSize: 12}]
+      let lStyle = { paddingBottom: 10, color: lcolor, fontSize: 12}
       title = utils.translate('Please click here to change signature')
       let {width, height} = value //utils.dimensions(params.component)
       let h = 70
@@ -822,7 +823,8 @@ var NewResourceMixin = {
     }
     else {
       let vStyle = { height: 55, paddingVertical: 10, flexDirection: 'row', justifyContent: 'space-between', margin: 15, borderBottomColor: '#cccccc', borderBottomWidth: 1}
-      let lStyle = [styles.labelStyle, { color: lcolor, fontSize: 20}]
+      // let lStyle = [styles.labelStyle, { color: lcolor, fontSize: 20}]
+      let lStyle = { color: lcolor, fontSize: 20}
       title = utils.translate('Please click here to sign')
       sig = <View style={vStyle}>
               <Text style={lStyle}>{title}</Text>
@@ -834,7 +836,7 @@ var NewResourceMixin = {
     }
     else {
       return <View style={st}>
-               <TouchableOpacity onPress={this.showSignatureView.bind(this, { prop, doSet, onSet: this.changeValue.bind(this, prop) })}>
+               <TouchableOpacity onPress={this.showSignatureView.bind(this, { prop, doSet, onSet: this.changeValue.bind(this) })}>
                  {sig}
                </TouchableOpacity>
             </View>
@@ -2024,7 +2026,8 @@ var styles= StyleSheet.create({
   },
   markdown: {
     backgroundColor: '#f7f7f7',
-    paddingVertical: 10,
+    padding: 10
+    // paddingVertical: 10,
   },
   container: {
     flex: 1
