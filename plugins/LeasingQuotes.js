cb854f1a4e993f4be5d850b79498866dcd02e898
pgmemk <ellen@tradle.io>
2022-02-28 22:53:15 -0500

    fix: bad return

--- plugins/LeasingQuotes.js
+++ plugins/LeasingQuotes.js
@@ -39,7 +39,7 @@ module.exports = function LeasingQuotes ({ models }) {
             return { name: p }
           })
       }
-      return { requestedProperties } || []
+      return { requestedProperties }
     }
   }
 }
