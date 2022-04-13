f32fd6761e5f07ff3172667706f65ecf7000a9f0
pgmemk <ellen@tradle.io>
2022-04-13 15:26:40 -0400

    feat: start application from prerequisite resource screen

--- Components/RowMixin.js
+++ Components/RowMixin.js
@@ -232,7 +232,7 @@ var RowMixin = {
     let routes = this.props.navigator.getCurrentRoutes()
     let msgWidth = utils.getMessageWidth(routes[routes.length - 1].component)
 
-    let isMyMessage = this.isMyMessage();
+    let isMyMessage = this.isMyMessage()
     let isShared = !isMyMessage  &&  this.isShared()
 
     let view = <View style={styles.sendStatus}>
@@ -349,6 +349,17 @@ var RowMixin = {
       this.setState({isChosen: true})
       this.props.chosen[id] = resource
     }
+  },
+  applyForProduct() {
+    let { resource } = this.props
+    let rType = utils.getType(resource)
+    let model = utils.getModel(rType);
+
+    Actions.applyForProduct({
+      provider: utils.getMe().organization,
+      _ref: utils.buildRef(resource),
+      product: model.prerequisiteFor
+    })
   }
 }
 
